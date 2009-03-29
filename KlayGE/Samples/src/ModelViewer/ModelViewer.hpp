65da6e2c88569bb536bc1eea932678e4df6db5c9
Minmin Gong <gongminmin@msn.com>
2009-03-29 08:12:33 +0000

    ModelViewer

---
+++ KlayGE/Samples/src/ModelViewer/ModelViewer.hpp
@@ -0,0 +1,62 @@
+#ifndef _SKINNEDMESH_HPP
+#define _SKINNEDMESH_HPP
+
+#include <KlayGE/App3D.hpp>
+#include <KlayGE/Font.hpp>
+#include <KlayGE/CameraController.hpp>
+#include <KlayGE/UI.hpp>
+#include "Model.hpp"
+
+class ModelViewerApp : public KlayGE::App3DFramework
+{
+public:
+	ModelViewerApp(std::string const & name, KlayGE::RenderSettings const & settings);
+
+private:
+	void InitObjects();
+	void OnResize(KlayGE::uint32_t width, KlayGE::uint32_t height);
+	KlayGE::uint32_t DoUpdate(KlayGE::uint32_t pass);
+
+	void OpenModel(std::string const & name);
+
+	void InputHandler(KlayGE::InputEngine const & sender, KlayGE::InputAction const & action);
+	void OpenHandler(KlayGE::UIButton const & sender);
+	void SaveAsHandler(KlayGE::UIButton const & sender);
+	void SkinnedHandler(KlayGE::UICheckBox const & sender);
+	void FrameChangedHandler(KlayGE::UISlider const & sender);
+	void PlayHandler(KlayGE::UICheckBox const & sender);
+	void FPSCameraHandler(KlayGE::UICheckBox const & sender);
+	void MeshChangedHandler(KlayGE::UIComboBox const & sender);
+	void VisualizeChangedHandler(KlayGE::UIComboBox const & sender);
+	void LineModeChangedHandler(KlayGE::UICheckBox const & sender);
+
+	KlayGE::FontPtr font_;
+
+	boost::shared_ptr<DetailedSkinnedModel> model_;
+	KlayGE::SceneObjectPtr axis_;
+	KlayGE::SceneObjectPtr grid_;
+
+	KlayGE::FirstPersonCameraController fpsController_;
+	KlayGE::TrackballCameraController tbController_;
+
+	float last_time_;
+	int frame_;
+
+	bool skinned_;
+	bool play_;
+	KlayGE::UIDialogPtr dialog_;
+	int id_open_;
+	int id_save_as_;
+	int id_skinned_;
+	int id_frame_static_;
+	int id_frame_slider_;
+	int id_play_;
+	int id_fps_camera_;
+	int id_mesh_;
+	int id_vertex_streams_;
+	int id_textures_;
+	int id_visualize_;
+	int id_line_mode_;
+};
+
+#endif		// _SKINNEDMESH_HPP
