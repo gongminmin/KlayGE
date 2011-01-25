0b7fa4fdaf376f13c2b0c825648c170ddce18d53
Ray Wang <wangray84@gmail.com>
2011-01-25 12:59:08 +0800

    ray: fix cmake support to be built smoothly with debug configuration

--- klayge/Build/cmake/CMakeModules/FindOggVorbis.cmake
+++ klayge/Build/cmake/CMakeModules/FindOggVorbis.cmake
@@ -4,10 +4,11 @@
 
 #  LIBOGG_FOUND, If false, do not try to use libogg.
 #  LIBOGG_INCLUDE_DIR, where to find ogg.h, etc.
-#  LIBOGG_LIBRARY_DEBUG, LIBOGG_LIBRARY, LIBOGG_LIBRARIES
+#  LIBOGG_LIBRARY_DEBUG, LIBOGG_LIBRARY
 
 FIND_PATH( LIBOGG_INCLUDE_DIR ogg/ogg.h
     PATHS
+    ${3RDPARTY_DIR}/include
     /usr/include
     /local/usr/include
 )
@@ -15,6 +16,7 @@ FIND_PATH( LIBOGG_INCLUDE_DIR ogg/ogg.h
 FIND_LIBRARY( LIBOGG_LIBRARY 
     NAMES libogg libogg_static
     PATHS
+    ${3RDPARTY_DIR}/lib
     /usr/lib
     /usr/lib64
     /local/usr/lib
@@ -24,16 +26,12 @@ FIND_LIBRARY( LIBOGG_LIBRARY
 FIND_LIBRARY( LIBOGG_LIBRARY_DEBUG 
     NAMES libogg_d libogg_static_d
     PATHS
+    ${3RDPARTY_DIR}/lib
     /usr/lib
     /usr/lib64
     /local/usr/lib
     /local/usr/lib64
 )
-
-SET(LIBOGG_LIBRARIES ${LIBOGG_LIBRARY})
-IF(LIBOGG_LIBRARY_DEBUG)
-    SET(LIBOGG_LIBRARIES "debug ${LIBOGG_LIBRARY_DEBUG} optimized ${LIBOGG_LIBRARY}")
-ENDIF(LIBOGG_LIBRARY_DEBUG)
 
 SET(LIBOGG_FOUND "NO")
 IF(LIBOGG_INCLUDE_DIR AND LIBOGG_LIBRARY)
@@ -42,9 +40,10 @@ ENDIF(LIBOGG_INCLUDE_DIR AND LIBOGG_LIBRARY)
 
 #  VORBIS_FOUND, If false, do not try to use vorbis.
 #  VORBIS_INCLUDE_DIR, where to find ogg.h, etc.
-#  VORBIS_LIBRARY_DEBUG, VORBIS_LIBRARY, VORBISFILE_LIBRARY_DEBUG, VORBISFILE_LIBRARY, VORBIS_LIBRARIES
+#  VORBIS_LIBRARY_DEBUG, VORBIS_LIBRARY, VORBISFILE_LIBRARY_DEBUG, VORBISFILE_LIBRARY
 FIND_PATH( VORBIS_INCLUDE_DIR vorbis/vorbisenc.h
     PATHS
+    ${3RDPARTY_DIR}/include
     /usr/include
     /local/usr/include
 )
@@ -52,6 +51,7 @@ FIND_PATH( VORBIS_INCLUDE_DIR vorbis/vorbisenc.h
 FIND_LIBRARY( VORBIS_LIBRARY 
     NAMES libvorbis libvorbis_static
     PATHS
+    ${3RDPARTY_DIR}/lib
     /usr/lib
     /usr/lib64
     /local/usr/lib
@@ -61,6 +61,7 @@ FIND_LIBRARY( VORBIS_LIBRARY
 FIND_LIBRARY( VORBIS_LIBRARY_DEBUG 
     NAMES libvorbis_d libvorbis_static_d
     PATHS
+    ${3RDPARTY_DIR}/lib
     /usr/lib
     /usr/lib64
     /local/usr/lib
@@ -70,6 +71,7 @@ FIND_LIBRARY( VORBIS_LIBRARY_DEBUG
 FIND_LIBRARY( VORBISFILE_LIBRARY 
     NAMES libvorbisfile libvorbisfile_static
     PATHS
+    ${3RDPARTY_DIR}/lib
     /usr/lib
     /usr/lib64
     /local/usr/lib
@@ -79,22 +81,12 @@ FIND_LIBRARY( VORBISFILE_LIBRARY
 FIND_LIBRARY( VORBISFILE_LIBRARY_DEBUG 
     NAMES libvorbisfile_d libvorbisfile_static_d
     PATHS
+    ${3RDPARTY_DIR}/lib
     /usr/lib
     /usr/lib64
     /local/usr/lib
     /local/usr/lib64
 )
-
-SET(VORBIS_LIBRARIES ${VORBIS_LIBRARY})
-IF(VORBIS_LIBRARY_DEBUG)
-    SET(VORBIS_LIBRARIES "debug ${VORBIS_LIBRARY_DEBUG} optimized ${VORBIS_LIBRARY}")
-ENDIF(VORBIS_LIBRARY_DEBUG)
-
-IF(VORBISFILE_LIBRARY_DEBUG)
-    SET(VORBIS_LIBRARIES ${VORBIS_LIBRARIES} "debug ${VORBISFILE_LIBRARY_DEBUG} optimized ${VORBISFILE_LIBRARY}")
-ELSE(VORBISFILE_LIBRARY_DEBUG)
-    SET(VORBIS_LIBRARIES ${VORBIS_LIBRARIES} ${VORBISFILE_LIBRARY})
-ENDIF(VORBISFILE_LIBRARY_DEBUG)
 
 SET(VORBIS_FOUND "NO")
 IF(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY)
