18252eb689b313c5b8c75f8475c9f4413fb83093
Minmin Gong <gongminmin@msn.com>
2011-05-29 22:32:07 +0800

    KlayGE: Samples: Dual Paraboloid Shadow Map support

--- KlayGE/Samples/src/ShadowCubeMap/ShadowCubeMap.cpp
+++ KlayGE/Samples/src/ShadowCubeMap/ShadowCubeMap.cpp
@@ -41,8 +41,8 @@ namespace
 	class ShadowMapped
 	{
 	public:
-		ShadowMapped(uint32_t shadow_map_size)
-			: shadow_map_size_(shadow_map_size)
+		explicit ShadowMapped(uint32_t shadow_map_size)
+			: shadow_map_size_(shadow_map_size), pass_index_(0)
 		{
 			FrameBufferPtr fb = Context::Instance().RenderFactoryInstance().MakeFrameBuffer();
 			flipping_ = static_cast<int32_t>(fb->RequiresFlipping() ? +1 : -1);
@@ -53,9 +53,11 @@ namespace
 			return light_view_ * light_proj_;
 		}
 
-		virtual void GenShadowMapPass(bool gen_sm)
+		virtual void GenShadowMapPass(bool gen_sm, bool dpsm, int pass_index)
 		{
 			gen_sm_pass_ = gen_sm;
+			dpsm_ = dpsm;
+			pass_index_ = pass_index;
 		}
 
 		void LightSrc(LightSourcePtr const & light_src)
@@ -69,12 +71,22 @@ namespace
 			light_view_ = app.ActiveCamera().ViewMatrix();
 			light_proj_ = app.ActiveCamera().ProjMatrix();
 
+			if (0 == pass_index_)
+			{
+				first_light_view_ = light_view_;
+			}
+
 			light_falloff_ = light_src->Falloff();
 		}
 
-		void ShadowMapTexture(TexturePtr const & cube_tex)
+		void CubeSMTexture(TexturePtr const & cube_tex)
 		{
 			sm_cube_tex_ = cube_tex;
+		}
+
+		void DPSMTexture(TexturePtr const & dual_tex)
+		{
+			sm_dual_tex_ = dual_tex;
 		}
 
 		void LampTexture(TexturePtr const & tex)
@@ -83,7 +95,7 @@ namespace
 		}
 
 	protected:
-		void OnRenderBegin(float4x4 const & model, RenderEffectPtr effect)
+		void OnRenderBegin(float4x4 const & model, RenderEffectPtr const & effect)
 		{
 			App3DFramework const & app = Context::Instance().AppInstance();
 
@@ -93,6 +105,13 @@ namespace
 			if (gen_sm_pass_)
 			{
 				*(effect->ParameterByName("mvp")) = model * this->LightViewProj();
+
+				if (dpsm_)
+				{
+					float4x4 mv = model * light_view_;
+					*(effect->ParameterByName("mv")) = mv;
+					*(effect->ParameterByName("far")) = app.ActiveCamera().FarPlane();
+				}
 			}
 			else
 			{
@@ -102,12 +121,22 @@ namespace
 				*(effect->ParameterByName("mvp")) = model * view * proj;
 				*(effect->ParameterByName("light_pos")) = light_pos_;
 
-				*(effect->ParameterByName("flipping")) = flipping_;
-
 				*(effect->ParameterByName("light_projective_tex")) = lamp_tex_;
 				*(effect->ParameterByName("shadow_cube_tex")) = sm_cube_tex_;
+				*(effect->ParameterByName("shadow_dual_tex")) = sm_dual_tex_;
 
 				*(effect->ParameterByName("light_falloff")) = light_falloff_;
+
+				*(effect->ParameterByName("dpsm")) = static_cast<int32_t>(dpsm_);
+				if (dpsm_)
+				{
+					*(effect->ParameterByName("flipping")) = -flipping_;
+					*(effect->ParameterByName("obj_model_to_light_view")) = model * first_light_view_;
+				}
+				else
+				{
+					*(effect->ParameterByName("flipping")) = flipping_;
+				}
 			}
 		}
 
@@ -117,11 +146,15 @@ namespace
 		int32_t flipping_;
 
 		bool gen_sm_pass_;
+		bool dpsm_;
+		int pass_index_;
 		TexturePtr sm_cube_tex_;
+		TexturePtr sm_dual_tex_;
 
 		float3 light_pos_;
 		float4x4 inv_light_model_;
 		float4x4 light_view_, light_proj_;
+		float4x4 first_light_view_;
 		float3 light_falloff_;
 
 		TexturePtr lamp_tex_;
@@ -203,13 +236,20 @@ namespace
 			model_matrix_ = model;
 		}
 
-		void GenShadowMapPass(bool gen_sm)
+		void GenShadowMapPass(bool gen_sm, bool dpsm, int pass_index)
 		{
-			ShadowMapped::GenShadowMapPass(gen_sm);
+			ShadowMapped::GenShadowMapPass(gen_sm, dpsm, pass_index);
 
 			if (gen_sm)
 			{
-				technique_ = effect_->TechniqueByName("GenShadowMap");
+				if (dpsm)
+				{
+					technique_ = effect_->TechniqueByName("GenDPShadowMap");
+				}
+				else
+				{
+					technique_ = effect_->TechniqueByName("GenCubeShadowMap");
+				}
 			}
 			else
 			{
@@ -320,7 +360,8 @@ int main()
 }
 
 ShadowCubeMap::ShadowCubeMap()
-				: App3DFramework("ShadowCubeMap")
+				: App3DFramework("ShadowCubeMap"),
+					sm_type_(0)
 {
 	ResLoader::Instance().AddPath("../../Samples/media/ShadowCubeMap");
 }
@@ -369,8 +410,8 @@ void ShadowCubeMap::InitObjects()
 		checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->LampTexture(lamp_tex_);
 	}
 
-	RenderViewPtr depth_view = rf.Make2DDepthStencilRenderView(SHADOW_MAP_SIZE, SHADOW_MAP_SIZE, EF_D16, 1, 0);
-	shadow_buffer_ = rf.MakeFrameBuffer();
+	RenderViewPtr depth_view = rf.Make2DDepthStencilRenderView(SHADOW_MAP_SIZE, SHADOW_MAP_SIZE, EF_D24S8, 1, 0);
+	shadow_cube_buffer_ = rf.MakeFrameBuffer();
 	ElementFormat fmt;
 	if (rf.RenderEngineInstance().DeviceCaps().rendertarget_format_support(EF_GR16F, 1, 0))
 	{
@@ -383,13 +424,27 @@ void ShadowCubeMap::InitObjects()
 		fmt = EF_ABGR16F;
 	}
 	shadow_tex_ = rf.MakeTexture2D(SHADOW_MAP_SIZE, SHADOW_MAP_SIZE, 1, 1, fmt, 1, 0, EAH_GPU_Read | EAH_GPU_Write, NULL);
-	shadow_buffer_->Attach(FrameBuffer::ATT_Color0, rf.Make2DRenderView(*shadow_tex_, 0, 0));
-	shadow_buffer_->Attach(FrameBuffer::ATT_DepthStencil, depth_view);
+	shadow_cube_buffer_->Attach(FrameBuffer::ATT_Color0, rf.Make2DRenderView(*shadow_tex_, 0, 0));
+	shadow_cube_buffer_->Attach(FrameBuffer::ATT_DepthStencil, depth_view);
 
 	shadow_cube_tex_ = rf.MakeTextureCube(SHADOW_MAP_SIZE, 1, 1, shadow_tex_->Format(), 1, 0, EAH_GPU_Read | EAH_GPU_Write, NULL);
+
+	for (int i = 0; i < 2; ++ i)
+	{
+		shadow_dual_texs_[i] = rf.MakeTexture2D(SHADOW_MAP_SIZE,  SHADOW_MAP_SIZE, 1, 1, fmt, 1, 0, EAH_GPU_Read | EAH_GPU_Write, NULL);
+		shadow_dual_view_[i] = rf.Make2DRenderView(*shadow_dual_texs_[i], 0, 0);
+
+		shadow_dual_buffers_[i] = rf.MakeFrameBuffer();
+		shadow_dual_buffers_[i]->GetViewport().camera->ProjParams(PI, 1, 0.1f, 500.0f);
+		shadow_dual_buffers_[i]->Attach(FrameBuffer::ATT_Color0, shadow_dual_view_[i]);
+		shadow_dual_buffers_[i]->Attach(FrameBuffer::ATT_DepthStencil, depth_view);
+	}
+	shadow_dual_tex_ = rf.MakeTexture2D(SHADOW_MAP_SIZE * 2,  SHADOW_MAP_SIZE, 1, 1, fmt, 1, 0, EAH_GPU_Read, NULL);
+
 	for (size_t i = 0; i < scene_objs_.size(); ++ i)
 	{
-		checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->ShadowMapTexture(shadow_cube_tex_);
+		checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->CubeSMTexture(shadow_cube_tex_);
+		checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->DPSMTexture(shadow_dual_tex_);
 	}
 
 	light_ = MakeSharedPtr<PointLightSource>();
@@ -410,7 +465,7 @@ void ShadowCubeMap::InitObjects()
 	
 		sm_filter_pps_[i]->InputPin(0, shadow_tex_);
 		sm_filter_pps_[i]->OutputPin(0, shadow_cube_tex_, 0, 0, i);
-		if (!shadow_buffer_->RequiresFlipping())
+		if (!shadow_cube_buffer_->RequiresFlipping())
 		{
 			switch (i)
 			{
@@ -445,14 +500,18 @@ void ShadowCubeMap::InitObjects()
 	id_min_variance_slider_ = dialog_->IDFromName("MinVarianceSlider");
 	id_bleeding_reduce_static_ = dialog_->IDFromName("BleedingReduceStatic");
 	id_bleeding_reduce_slider_ = dialog_->IDFromName("BleedingReduceSlider");
+	id_sm_type_static_ = dialog_->IDFromName("SMStatic");
+	id_sm_type_combo_ = dialog_->IDFromName("SMCombo");
 	id_ctrl_camera_ = dialog_->IDFromName("CtrlCamera");
 
 	dialog_->Control<UISlider>(id_min_variance_slider_)->OnValueChangedEvent().connect(boost::bind(&ShadowCubeMap::MinVarianceChangedHandler, this, _1));
 	dialog_->Control<UISlider>(id_bleeding_reduce_slider_)->OnValueChangedEvent().connect(boost::bind(&ShadowCubeMap::BleedingReduceChangedHandler, this, _1));
+	dialog_->Control<UIComboBox>(id_sm_type_combo_)->OnSelectionChangedEvent().connect(boost::bind(&ShadowCubeMap::SMTypeChangedHandler, this, _1));
 	dialog_->Control<UICheckBox>(id_ctrl_camera_)->OnChangedEvent().connect(boost::bind(&ShadowCubeMap::CtrlCameraHandler, this, _1));
 
 	this->MinVarianceChangedHandler(*dialog_->Control<UISlider>(id_min_variance_slider_));
 	this->BleedingReduceChangedHandler(*dialog_->Control<UISlider>(id_bleeding_reduce_slider_));
+	this->SMTypeChangedHandler(*dialog_->Control<UIComboBox>(id_sm_type_combo_));
 }
 
 void ShadowCubeMap::OnResize(uint32_t width, uint32_t height)
@@ -490,6 +549,11 @@ void ShadowCubeMap::BleedingReduceChangedHandler(KlayGE::UISlider const & sender
 	}
 }
 
+void ShadowCubeMap::SMTypeChangedHandler(KlayGE::UIComboBox const & sender)
+{
+	sm_type_ = sender.GetSelectedIndex();
+}
+
 void ShadowCubeMap::CtrlCameraHandler(KlayGE::UICheckBox const & sender)
 {
 	if (sender.GetChecked())
@@ -518,49 +582,84 @@ uint32_t ShadowCubeMap::DoUpdate(uint32_t pass)
 {
 	RenderEngine& renderEngine = Context::Instance().RenderFactoryInstance().RenderEngineInstance();
 
-	if (pass > 0)
+	if (0 == sm_type_)
 	{
-		sm_filter_pps_[pass - 1]->Apply();
+		switch (pass)
+		{
+		case 0:
+		case 1:
+			{
+				float3 pos = light_->Position();
+				float3 lookat = light_->Position() + ((0 == pass) ? 1.0f : -1.0f) * light_->Direction();
+				shadow_dual_buffers_[pass]->GetViewport().camera->ViewParams(pos, lookat);
+
+				renderEngine.BindFrameBuffer(shadow_dual_buffers_[pass]);
+				renderEngine.CurFrameBuffer()->Clear(FrameBuffer::CBM_Color | FrameBuffer::CBM_Depth, Color(0.0f, 0.0f, 0.0f, 1), 1.0f, 0);
+
+				for (size_t i = 0; i < scene_objs_.size(); ++ i)
+				{
+					checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->GenShadowMapPass(true, 0 == sm_type_, pass);
+					checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->LightSrc(light_);
+				}
+			}
+			return App3DFramework::URV_Need_Flush;
+
+		default:
+			{
+				renderEngine.BindFrameBuffer(FrameBufferPtr());
+				renderEngine.CurFrameBuffer()->Clear(FrameBuffer::CBM_Color | FrameBuffer::CBM_Depth, Color(0.2f, 0.4f, 0.6f, 1), 1.0f, 0);
+
+				shadow_dual_texs_[0]->CopyToSubTexture2D(*shadow_dual_tex_, 0, 0, 0, 0, SHADOW_MAP_SIZE, SHADOW_MAP_SIZE, 0, 0, 0, 0, SHADOW_MAP_SIZE, SHADOW_MAP_SIZE);
+				shadow_dual_texs_[1]->CopyToSubTexture2D(*shadow_dual_tex_, 0, 0, SHADOW_MAP_SIZE, 0, SHADOW_MAP_SIZE, SHADOW_MAP_SIZE, 0, 0, 0, 0, SHADOW_MAP_SIZE, SHADOW_MAP_SIZE);
+
+				for (size_t i = 0; i < scene_objs_.size(); ++ i)
+				{
+					checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->GenShadowMapPass(false, 0 == sm_type_, pass);
+				}
+			}
+			return App3DFramework::URV_Need_Flush | App3DFramework::URV_Finished;
+		}
 	}
-
-	switch (pass)
+	else
 	{
-	case 0:
+		if (pass > 0)
 		{
-			for (size_t i = 0; i < scene_objs_.size(); ++ i)
-			{
-				checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->GenShadowMapPass(true);
-			}
+			sm_filter_pps_[pass - 1]->Apply();
 		}
 
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-	case 5:
+		switch (pass)
 		{
-			renderEngine.BindFrameBuffer(shadow_buffer_);
-			renderEngine.CurFrameBuffer()->Clear(FrameBuffer::CBM_Color | FrameBuffer::CBM_Depth, Color(0.2f, 0.4f, 0.6f, 1), 1.0f, 0);
-
-			shadow_buffer_->GetViewport().camera = light_->SMCamera(pass);
-
-			for (size_t i = 0; i < scene_objs_.size(); ++ i)
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
 			{
-				checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->LightSrc(light_);
+				renderEngine.BindFrameBuffer(shadow_cube_buffer_);
+				renderEngine.CurFrameBuffer()->Clear(FrameBuffer::CBM_Color | FrameBuffer::CBM_Depth, Color(0.0f, 0.0f, 0.0f, 1), 1.0f, 0);
+
+				shadow_cube_buffer_->GetViewport().camera = light_->SMCamera(pass);
+
+				for (size_t i = 0; i < scene_objs_.size(); ++ i)
+				{
+					checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->GenShadowMapPass(true, 0 == sm_type_, pass);
+					checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->LightSrc(light_);
+				}
 			}
-		}
-		return App3DFramework::URV_Need_Flush;
+			return App3DFramework::URV_Need_Flush;
 
-	default:
-		{
-			renderEngine.BindFrameBuffer(FrameBufferPtr());
-			renderEngine.CurFrameBuffer()->Clear(FrameBuffer::CBM_Color | FrameBuffer::CBM_Depth, Color(0.2f, 0.4f, 0.6f, 1), 1.0f, 0);
-
-			for (size_t i = 0; i < scene_objs_.size(); ++ i)
+		default:
 			{
-				checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->GenShadowMapPass(false);
+				renderEngine.BindFrameBuffer(FrameBufferPtr());
+				renderEngine.CurFrameBuffer()->Clear(FrameBuffer::CBM_Color | FrameBuffer::CBM_Depth, Color(0.2f, 0.4f, 0.6f, 1), 1.0f, 0);
+
+				for (size_t i = 0; i < scene_objs_.size(); ++ i)
+				{
+					checked_pointer_cast<OccluderMesh>(scene_objs_[i]->GetRenderable())->GenShadowMapPass(false, 0 == sm_type_, pass);
+				}
 			}
+			return App3DFramework::URV_Need_Flush | App3DFramework::URV_Finished;
 		}
-		return App3DFramework::URV_Need_Flush | App3DFramework::URV_Finished;
 	}
 }
--- KlayGE/Samples/src/ShadowCubeMap/ShadowCubeMap.hpp
+++ KlayGE/Samples/src/ShadowCubeMap/ShadowCubeMap.hpp
@@ -23,6 +23,7 @@ private:
 	void InputHandler(KlayGE::InputEngine const & sender, KlayGE::InputAction const & action);
 	void MinVarianceChangedHandler(KlayGE::UISlider const & sender);
 	void BleedingReduceChangedHandler(KlayGE::UISlider const & sender);
+	void SMTypeChangedHandler(KlayGE::UIComboBox const & sender);
 	void CtrlCameraHandler(KlayGE::UICheckBox const & sender);
 
 	KlayGE::FontPtr font_;
@@ -31,21 +32,30 @@ private:
 
 	KlayGE::FirstPersonCameraController fpcController_;
 
-	KlayGE::FrameBufferPtr shadow_buffer_;
+	KlayGE::FrameBufferPtr shadow_cube_buffer_;
 	KlayGE::TexturePtr shadow_tex_;
 	KlayGE::TexturePtr shadow_cube_tex_;
 	KlayGE::PostProcessPtr sm_filter_pps_[6];
+
+	KlayGE::FrameBufferPtr shadow_dual_buffers_[2];
+	KlayGE::TexturePtr shadow_dual_texs_[2];
+	KlayGE::RenderViewPtr shadow_dual_view_[2];
+	KlayGE::TexturePtr shadow_dual_tex_;
 
 	KlayGE::TexturePtr lamp_tex_;
 
 	KlayGE::SceneObjectPtr light_proxy_;
 	KlayGE::LightSourcePtr light_;
 
+	int sm_type_;
+
 	KlayGE::UIDialogPtr dialog_;
 	int id_min_variance_static_;
 	int id_min_variance_slider_;
 	int id_bleeding_reduce_static_;
 	int id_bleeding_reduce_slider_;
+	int id_sm_type_static_;
+	int id_sm_type_combo_;
 	int id_ctrl_camera_;
 };
 
