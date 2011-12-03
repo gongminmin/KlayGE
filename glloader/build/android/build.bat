88136394fdc3472cc2ea9311570df9c0f57f0615
Minmin Gong <gongminmin@msn.com>
2011-12-03 23:05:46 +0800

    External: boost get compiled by NDK r7. glloader: Workaround for NDK r7.

--- glloader/build/android/build.bat
+++ glloader/build/android/build.bat
@@ -1,3 +1,11 @@
 set PATH=%PATH%;%ANDROID_NDK%
 
+REM Workaround for NDK r7
+mkdir obj
+mkdir obj\local
+mkdir obj\local\armeabi
+mkdir obj\local\armeabi-v7a
+copy "%ANDROID_NDK%\sources\cxx-stl\gnu-libstdc++\libs\armeabi\libgnustl_static.a" obj\local\armeabi\
+copy "%ANDROID_NDK%\sources\cxx-stl\gnu-libstdc++\libs\armeabi-v7a\libgnustl_static.a" obj\local\armeabi-v7a\
+
 ndk-build
