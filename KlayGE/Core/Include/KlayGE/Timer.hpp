8e2cf35b635116d98426fde99aa83ec59a7b9406
Minmin Gong <gongminmin@msn.com>
2011-12-17 18:14:23 +0800

    KlayGE: Core: Fix Timer under android

--- KlayGE/Core/Include/KlayGE/Timer.hpp
+++ KlayGE/Core/Include/KlayGE/Timer.hpp
@@ -15,12 +15,10 @@
 
 #pragma once
 
-#include <limits>
 #ifdef KLAYGE_PLATFORM_WINDOWS
 	#include <windows.h>
 #else
 	#include <ctime>
-	#include <limits>
 #endif
 
 namespace KlayGE
@@ -28,63 +26,22 @@ namespace KlayGE
 	class KLAYGE_CORE_API Timer
 	{
 	public:
-		Timer()
-		{
-			if (0 == cps_)
-			{
-#ifdef KLAYGE_PLATFORM_WINDOWS
-				LARGE_INTEGER frequency;
-				QueryPerformanceFrequency(&frequency);
-				cps_ = static_cast<uint64_t>(frequency.QuadPart);
-#else
-				cps_ = CLOCKS_PER_SEC;
-#endif
-			}
-
-			this->restart();
-		} // postcondition: elapsed()==0
-		void restart()
-		{
-			start_time_ = this->current_time();
-		} // postcondition: elapsed()==0
+		Timer(); // postcondition: elapsed()==0
+		void restart(); // postcondition: elapsed()==0
 
 		// return elapsed time in seconds
-		double elapsed() const
-		{
-			return this->current_time() - start_time_;
-		}
+		double elapsed() const;
 
 		// return estimated maximum value for elapsed()
-		double elapsed_max() const
-		{
-#ifdef KLAYGE_PLATFORM_WINDOWS
-			return static_cast<double>(std::numeric_limits<uint64_t>::max()) / cps_ - start_time_;
-#else
-			return static_cast<double>(std::numeric_limits<std::clock_t>::max()) / cps_ - start_time_;
-#endif
-		}
+		double elapsed_max() const;
 
 		// return minimum value for elapsed()
-		double elapsed_min() const
-		{
-			return 1.0 / cps_;
-		}
+		double elapsed_min() const;
 
-		double current_time() const
-		{
-#ifdef KLAYGE_PLATFORM_WINDOWS
-			LARGE_INTEGER count;
-			QueryPerformanceCounter(&count);
-			return static_cast<double>(count.QuadPart) / cps_;
-#else
-			return static_cast<double>(std::clock()) / cps_;
-#endif
-		}
+		double current_time() const;
 
 	private:
 		double start_time_;
-
-		static uint64_t cps_;
 	};
 }
 
