6b04b166d689cfbd533b91085a7d9a48a3cea453
Minmin Gong <gongminmin@msn.com>
2012-03-04 01:30:46 +0800

    KlayGE: Build: Remove vc9 and vc10 project files.

--- KlayGE/Build/vc-10_0/build_all.bat
+++
@@ -1,9 +0,0 @@
-CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
-devenv KlayGE.sln /Build "Debug|Win32"
-devenv KlayGE.sln /Build "Release|Win32"
-
-CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86_amd64
-devenv KlayGE.sln /Build "Debug|x64"
-devenv KlayGE.sln /Build "Release|x64"
-
-if not "%1" == "-q" pause
