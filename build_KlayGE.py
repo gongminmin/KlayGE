81dc114490baa65bdb82528991082a2f6e32210a
Minmin Gong <gongminmin@msn.com>
2012-03-04 15:13:20 +0800

    KlayGE: Enhance the building script.

---
+++ build_KlayGE.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+#-*- coding: ascii -*-
+
+from __future__ import print_function
+import os, sys
+from blib_util import *
+
+def build_KlayGE(compiler_name, compiler_version, compiler_arch, generator_name):
+	curdir = os.path.abspath(os.curdir)
+
+	build_dir = "KlayGE/Build/%s-%d_0-%s" % (compiler_name, compiler_version, compiler_arch)
+	if not os.path.exists(build_dir):
+		os.makedirs(build_dir)
+
+	os.chdir(build_dir)
+
+	cmake_cmd = batch_command()
+	cmake_cmd.add_command('cmake -G "%s" %s' % (generator_name, "../cmake"))
+	cmake_cmd.execute()
+
+	config_list = ("Debug", "Release")
+
+	build_cmd = batch_command()
+	build_cmd.add_command('CALL "%%VS%d0COMNTOOLS%%..\\..\\VC\\vcvarsall.bat" %s' % (compiler_version, compiler_arch))
+	for config in config_list:
+		build_cmd.add_command('devenv KlayGE.sln /Build %s /project ALL_BUILD' % config)
+		build_cmd.add_command('devenv KlayGE.sln /Build %s /project Install' % config)
+	build_cmd.execute()
+
+	os.chdir(curdir)
+
+def build_Samples(compiler_name, compiler_version, compiler_arch, generator_name):
+	curdir = os.path.abspath(os.curdir)
+
+	build_dir = "KlayGE/Samples/build/%s-%d_0-%s" % (compiler_name, compiler_version, compiler_arch)
+	if not os.path.exists(build_dir):
+		os.makedirs(build_dir)
+
+	os.chdir(build_dir)
+
+	cmake_cmd = batch_command()
+	cmake_cmd.add_command('cmake -G "%s" %s' % (generator_name, "../cmake"))
+	cmake_cmd.execute()
+
+	config_list = ("Debug", "Release")
+
+	build_cmd = batch_command()
+	build_cmd.add_command('CALL "%%VS%d0COMNTOOLS%%..\\..\\VC\\vcvarsall.bat" %s' % (compiler_version, compiler_arch))
+	for config in config_list:
+		build_cmd.add_command('devenv Samples.sln /Build %s /project ALL_BUILD' % config)
+		build_cmd.add_command('devenv Samples.sln /Build %s /project Install' % config)
+	build_cmd.execute()
+
+	os.chdir(curdir)
+
+def build_Tools(compiler_name, compiler_version, compiler_arch, generator_name):
+	curdir = os.path.abspath(os.curdir)
+
+	build_dir = "KlayGE/Tools/build/%s-%d_0-%s" % (compiler_name, compiler_version, compiler_arch)
+	if not os.path.exists(build_dir):
+		os.makedirs(build_dir)
+
+	os.chdir(build_dir)
+
+	cmake_cmd = batch_command()
+	cmake_cmd.add_command('cmake -G "%s" %s' % (generator_name, "../cmake"))
+	cmake_cmd.execute()
+
+	config_list = ("Debug", "Release")
+
+	build_cmd = batch_command()
+	build_cmd.add_command('CALL "%%VS%d0COMNTOOLS%%..\\..\\VC\\vcvarsall.bat" %s' % (compiler_version, compiler_arch))
+	for config in config_list:
+		build_cmd.add_command('devenv Tools.sln /Build %s /project ALL_BUILD' % config)
+		build_cmd.add_command('devenv Tools.sln /Build %s /project Install' % config)
+	build_cmd.execute()
+
+	os.chdir(curdir)
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		cfg = sys.argv[1]
+	else:
+		cfg = ""
+
+	compiler_info = get_compiler_info(cfg)
+
+	if 0 == len(compiler_info):
+		print("Wrong configuration\n")
+		sys.exit(1)
+
+	print("Building KlayGE...")
+	for arch in compiler_info[2]:
+		build_KlayGE(compiler_info[0], compiler_info[1], arch[0], arch[1])
+
+	print("Building Samples...")
+	for arch in compiler_info[2]:
+		build_Samples(compiler_info[0], compiler_info[1], arch[0], arch[1])
+
+	print("Building Tools...")
+	for arch in compiler_info[2]:
+		build_Tools(compiler_info[0], compiler_info[1], arch[0], arch[1])
--- build_all.py
+++ build_all.py
@@ -2,10 +2,12 @@
 #-*- coding: ascii -*-
 
 from __future__ import print_function
-import os
-import sys
+import os, sys
 from build_external import build_external_libs
-from build_external import copy_to_dst
+from blib_util import *
+from build_glloader import *
+from build_kfont import *
+from build_KlayGE import *
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
@@ -13,74 +15,32 @@ if __name__ == "__main__":
 	else:
 		cfg = ""
 
-	env = os.environ
+	compiler_info = get_compiler_info(cfg)
 
-	if "" == cfg:
-		if "VS100COMNTOOLS" in env:
-			cfg = "vc10"
-		elif "VS90COMNTOOLS" in env:
-			cfg = "vc9"
-		elif "VS80COMNTOOLS" in env:
-			cfg = "vc8"
-		elif os.path.exists("C:\MinGW\bin\gcc.exe"):
-			cfg = "mingw"
-
-	if "vc10" == cfg:
-		compiler_name = "vc"
-		compiler_version = 10
-		ide_name = "VS"
-		ide_version = 2010
-	elif "vc9" == cfg:
-		compiler_name = "vc"
-		compiler_version = 9
-		ide_name = "VS"
-		ide_version = 2008
-	else:
+	if 0 == len(compiler_info):
 		print("Wrong configuration\n")
 		sys.exit(1)
-
-	dst_dir_x86 = "KlayGE/bin/win_x86/"
-	dst_dir_x64 = "KlayGE/bin/win_x64/"
-
 
 	print("Building external libs...")
 	build_external_libs(cfg)
 
 	print("Building glloader...")
-	os.chdir("glloader/build/%s-%d_0" % (compiler_name, compiler_version))
-	os.system("build_all.bat -q")
-	os.chdir("../../../")
-
-	copy_to_dst("glloader/lib/Win32/glloader_x86.dll", dst_dir_x86)
-	copy_to_dst("glloader/lib/Win32/glloader_x86_d.dll", dst_dir_x86)
-	copy_to_dst("glloader/lib/Win32/glloader_es_x86.dll", dst_dir_x86)
-	copy_to_dst("glloader/lib/Win32/glloader_es_x86_d.dll", dst_dir_x86)
-	copy_to_dst("glloader/lib/x64/glloader_x64.dll", dst_dir_x64)
-	copy_to_dst("glloader/lib/x64/glloader_x64_d.dll", dst_dir_x64)
-	copy_to_dst("glloader/lib/x64/glloader_es_x64.dll", dst_dir_x64)
-	copy_to_dst("glloader/lib/x64/glloader_es_x64_d.dll", dst_dir_x64)
+	for arch in compiler_info[2]:
+		build_glloader(compiler_info[0], compiler_info[1], arch[0], arch[1])
 
 	print("Building kfont...")
-	os.chdir("kfont/build/%s-%d_0" % (compiler_name, compiler_version))
-	os.system("build_all.bat -q")
-	os.chdir("../../../")
-
-	copy_to_dst("kfont/lib/Win32/kfont_x86.dll", dst_dir_x86)
-	copy_to_dst("kfont/lib/Win32/kfont_x86_d.dll", dst_dir_x86)
-	copy_to_dst("kfont/lib/x64/kfont_x64.dll", dst_dir_x64)
-	copy_to_dst("kfont/lib/x64/kfont_x64_d.dll", dst_dir_x64)
+	for arch in compiler_info[2]:
+		build_kfont(compiler_info[0], compiler_info[1], arch[0], arch[1])
 
 	print("Building KlayGE...")
-	os.chdir("KlayGE/Build/%s-%d_0" % (compiler_name, compiler_version))
-	os.system("build_all.bat -q")
-	os.chdir("../../../")
+	for arch in compiler_info[2]:
+		build_KlayGE(compiler_info[0], compiler_info[1], arch[0], arch[1])
 
 	print("Building KlayGE Samples...")
-	os.chdir("KlayGE/Samples/build/%s-%d_0" % (compiler_name, compiler_version))
-	os.system("build_all.bat -q")
-	os.chdir("../../../../")
+	for arch in compiler_info[2]:
+		build_Samples(compiler_info[0], compiler_info[1], arch[0], arch[1])
 
 	print("Building KlayGE Tools...")
-	os.chdir("KlayGE/Tools/build/%s-%d_0" % (compiler_name, compiler_version))
-	os.system("build_all.bat -q")
-	os.chdir("../../../../")
+	for arch in compiler_info[2]:
+		build_Tools(compiler_info[0], compiler_info[1], arch[0], arch[1])
+
