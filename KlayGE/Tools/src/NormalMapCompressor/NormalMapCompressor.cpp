176cb129346e8ff85e2b76ede2b7948f99b6b877
Minmin Gong <gongminmin@msn.com>
2012-09-24 22:33:43 +0800

    KlayGE: Tools: Enhanced NormalMapCompressor to take more input format.

--- KlayGE/Tools/src/NormalMapCompressor/NormalMapCompressor.cpp
+++ KlayGE/Tools/src/NormalMapCompressor/NormalMapCompressor.cpp
@@ -29,14 +29,9 @@ namespace
 		}
 	}
 
-	void CompressNormalMapSubresource(uint32_t width, uint32_t height, ElementFormat in_format, 
-		ElementInitData const & in_data, ElementFormat new_format, ElementInitData& new_data, std::vector<uint8_t>& new_data_block)
+	void CompressNormalMapSubresource(uint32_t width, uint32_t height, std::vector<Color>& in_data,
+		ElementFormat new_format, ElementInitData& new_data, std::vector<uint8_t>& new_data_block)
 	{
-		UNREF_PARAM(in_format);
-		BOOST_ASSERT(EF_ARGB8 == in_format);
-
-		uint8_t const * normals = static_cast<uint8_t const *>(in_data.data);
-
 		new_data.row_pitch = (width + 3) / 4 * 16;
 		new_data.slice_pitch = new_data.row_pitch * (height + 3) / 4;
 		new_data_block.resize(new_data.slice_pitch);
@@ -55,8 +50,14 @@ namespace
 				{
 					for (int x = 0; x < 4; ++ x)
 					{
-						uncom_y[y * 4 + x] = normals[((y_base + y) * width + (x_base + x)) * 4 + 1];
-						uncom_x[y * 4 + x] = normals[((y_base + y) * width + (x_base + x)) * 4 + 2];
+						float3 n;
+						n.x() = in_data[(y_base + y) * width + (x_base + x)].r() * 2 - 1;
+						n.y() = in_data[(y_base + y) * width + (x_base + x)].g() * 2 - 1;
+						n.z() = in_data[(y_base + y) * width + (x_base + x)].b() * 2 - 1;
+						n = MathLib::normalize(n);
+
+						uncom_x[y * 4 + x] = static_cast<uint8_t>(MathLib::clamp(static_cast<int>((n.x() * 0.5f + 0.5f) * 255.0f + 0.5f), 0, 255));
+						uncom_y[y * 4 + x] = static_cast<uint8_t>(MathLib::clamp(static_cast<int>((n.y() * 0.5f + 0.5f) * 255.0f + 0.5f), 0, 255));
 					}
 				}
 
@@ -182,10 +183,23 @@ namespace
 		std::vector<uint8_t> in_data_block;
 		LoadTexture(in_file, in_type, in_width, in_height, in_depth, in_num_mipmaps, in_array_size, in_format, in_data, in_data_block);
 
-		if (in_format != EF_ARGB8)
+		uint32_t const elem_size = NumFormatBytes(in_format);
+
+		std::vector<std::vector<Color> > in_color(in_data.size());
+		for (size_t sub_res = 0; sub_res < in_color.size(); ++ sub_res)
 		{
-			cout << "Unsupported texture format" << endl;
-			return;
+			uint32_t the_width = in_data[sub_res].row_pitch / elem_size;
+			uint32_t the_height = in_data[sub_res].slice_pitch / in_data[sub_res].row_pitch;
+			in_color[sub_res].resize(the_width * the_height);
+
+			uint8_t const * src = static_cast<uint8_t const *>(in_data[sub_res].data);
+			Color* dst = &in_color[sub_res][0];
+			for (uint32_t y = 0; y < the_height; ++ y)
+			{
+				ConvertToABGR32F(in_format, src, the_width, dst);
+				src += in_data[sub_res].row_pitch;
+				dst += the_width;
+			}
 		}
 
 		std::vector<ElementInitData> new_data(in_data.size());
@@ -196,7 +210,7 @@ namespace
 			uint32_t the_width = in_data[sub_res].row_pitch / 4;
 			uint32_t the_height = in_data[sub_res].slice_pitch / in_data[sub_res].row_pitch;
 
-			CompressNormalMapSubresource(the_width, the_height, in_format, in_data[sub_res], new_format, new_data[sub_res], new_data_block[sub_res]);
+			CompressNormalMapSubresource(the_width, the_height, in_color[sub_res], new_format, new_data[sub_res], new_data_block[sub_res]);
 		}
 
 		SaveTexture(out_file, in_type, in_width, in_height, in_depth, in_num_mipmaps, in_array_size, new_format, new_data);
