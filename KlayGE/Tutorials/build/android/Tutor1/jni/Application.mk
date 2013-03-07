70ded50598193a1267cd73feea42d6fc8b6eca56
Minmin Gong <gongminmin@msn.com>
2013-03-07 14:06:29 +0800

    Build: Enable C++11 in android NDK projects.

--- KlayGE/Tutorials/build/android/Tutor1/jni/Application.mk
+++ KlayGE/Tutorials/build/android/Tutor1/jni/Application.mk
@@ -6,6 +6,6 @@ APP_OPTIM := release
 
 APP_PLATFORM 	:= android-9
 APP_STL 		:= gnustl_static
-APP_CPPFLAGS 	:= -fexceptions -frtti
+APP_CPPFLAGS 	:= -std=c++11 -fexceptions -frtti
 APP_ABI 		:= armeabi armeabi-v7a x86
 APP_MODULES     := Tutor1
