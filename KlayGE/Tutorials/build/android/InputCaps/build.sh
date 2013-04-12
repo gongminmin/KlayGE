af36a2f101c9a94a32d59b74e7d2c2b4d0d986f4
Minmin Gong <gongminmin@msn.com>
2013-04-12 08:42:33 +0800

    KlayGE: Tutorials: Add android and codelite project files for InputCaps.

---
+++ KlayGE/Tutorials/build/android/InputCaps/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+$ANDROID_NDK/ndk-build -j 3
+
+if [ ! -d assets ]; then
+	mkdir assets
+fi
+rm -rf assets\*.*
+
+cp ../../../../bin/android_armeabi/KlayGE.cfg assets
+cp ../../../../media/Fonts/gkai00mp.kfont assets
+cp ../../../../media/RenderFX/Font.kfx assets
+
+ant debug
+adb install -r bin/KlayGE_InputCaps-debug.apk
