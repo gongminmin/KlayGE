5646f4e8139beb2c1f110cb635903a2681b98a0b
Minmin Gong <gongminmin@msn.com>
2013-11-06 22:18:15 +0800

    KlayGE: Tools: Generate fBm textures in NoiseTexGen.

--- KlayGE/Tools/src/NoiseTexGen/NoiseTexGen.cpp
+++ KlayGE/Tools/src/NoiseTexGen/NoiseTexGen.cpp
@@ -1,5 +1,6 @@
 #include <KlayGE/KlayGE.hpp>
 #include <KlayGE/Texture.hpp>
+#include <KFL/Noise.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -161,7 +162,7 @@ static uint8_t simplex[][4] =
 	{ 3, 2, 1, 0 }
 };
 
-int main()
+void GenSimplexPerm()
 {
 	std::vector<uint8_t> perm_2d(256 * 256);
 	for (int y = 0; y < 256; ++ y)
@@ -171,7 +172,7 @@ int main()
 			perm_2d[y * 256 + x] = permutation[(x + permutation[y & 255]) & 255];
 		}
 	}
-	
+
 	std::vector<ElementInitData> init_data(1);
 
 	init_data[0].data = simplex;
@@ -213,6 +214,80 @@ int main()
 	init_data[0].slice_pitch = static_cast<uint32_t>(grad_perm.size() * sizeof(grad_perm[0]));
 	SaveTexture("noise_grad4_perm.dds", Texture::TT_2D,
 		256, 256, 1, 1, 1, EF_ABGR8, init_data);
+}
+
+void GenfBmTexs()
+{
+	uint32_t const TEX_SIZE = 512;
+	float const STRIDE = 8;
+
+	MathLib::SimplexNoise<float> noiser = MathLib::SimplexNoise<float>::Instance();
+
+	std::vector<float> fdata(TEX_SIZE * TEX_SIZE);
+	float min_v = +1e10f;
+	float max_v = -1e10f;
+	for (uint32_t y = 0; y < TEX_SIZE; ++ y)
+	{
+		for (uint32_t x = 0; x < TEX_SIZE; ++ x)
+		{
+			float v = noiser.tileable_fBm((x + 0.5f) / TEX_SIZE * STRIDE, (y + 0.5f) / TEX_SIZE * STRIDE, STRIDE, STRIDE, 5, 2, 0.5f);
+			fdata[y * TEX_SIZE + x] = v;
+			min_v = std::min(min_v, v);
+			max_v = std::max(max_v, v);
+		}
+	}
+	float inv_range = 1 / (max_v - min_v);
+	std::vector<uint8_t> data(TEX_SIZE * TEX_SIZE);
+	for (uint32_t i = 0; i < data.size(); ++ i)
+	{
+		float v = (fdata[i] - min_v) * inv_range;
+		data[i] = static_cast<uint8_t>(MathLib::clamp(static_cast<int>(v * 255 + 0.5f), 0, 255));
+	}
+	std::vector<ElementInitData> init_data(1);
+	init_data[0].data = &data[0];
+	init_data[0].row_pitch = TEX_SIZE;
+	init_data[0].slice_pitch = TEX_SIZE * TEX_SIZE;
+	SaveTexture("fBm5_tex.dds", Texture::TT_2D,
+		TEX_SIZE, TEX_SIZE, 1, 1, 1, EF_R8, init_data);
+
+	system("Mipmapper fBm5_tex.dds");
+	system("TexCompressor BC4 fBm5_tex.dds");
+
+	std::vector<float3> fdata3(TEX_SIZE * TEX_SIZE);
+	for (uint32_t y = 0; y < TEX_SIZE; ++ y)
+	{
+		for (uint32_t x = 0; x < TEX_SIZE; ++ x)
+		{
+			float f0 = fdata[y * TEX_SIZE + x];
+			float d = 2;
+			float fx = noiser.tileable_fBm((x + d + 0.5f) / TEX_SIZE * STRIDE, (y + 0.5f) / TEX_SIZE * STRIDE, STRIDE, STRIDE, 5, 2, 0.5f);
+			float fy = noiser.tileable_fBm((x + 0.5f) / TEX_SIZE * STRIDE, (y + d + 0.5f) / TEX_SIZE * STRIDE, STRIDE, STRIDE, 5, 2, 0.5f);
+			fdata3[y * TEX_SIZE + x] = MathLib::normalize(float3(fx - f0, fy - f0, STRIDE * 16 / TEX_SIZE)) * 0.5f + 0.5f;
+		}
+	}
+	std::vector<uint32_t> data3(TEX_SIZE * TEX_SIZE);
+	for (uint32_t i = 0; i < fdata3.size(); ++ i)
+	{
+		data3[i] = (static_cast<uint8_t>(MathLib::clamp(static_cast<int>(fdata3[i].x() * 255 + 0.5f), 0, 255)) << 0)
+			| (static_cast<uint8_t>(MathLib::clamp(static_cast<int>(fdata3[i].y() * 255 + 0.5f), 0, 255)) << 8);
+	}
+	init_data[0].data = &data3[0];
+	init_data[0].row_pitch = TEX_SIZE * sizeof(uint32_t);
+	init_data[0].slice_pitch = TEX_SIZE * sizeof(uint32_t)* TEX_SIZE;
+	SaveTexture("fBm5_grad_tex.dds", Texture::TT_2D,
+		TEX_SIZE, TEX_SIZE, 1, 1, 1, EF_ABGR8, init_data);
+
+	system("Mipmapper fBm5_grad_tex.dds");
+	system("TexCompressor BC5 fBm5_grad_tex.dds");
+}
+
+int main()
+{
+	cout << "Generating textures for simplex..." << endl;
+	GenSimplexPerm();
+
+	cout << "Generating fBm textures..." << endl;
+	GenfBmTexs();
 
 	cout << "DONE" << endl;
 
