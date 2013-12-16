e0684f15485f10b39960fc42dd7d519ac4d7771e
Minmin Gong <gongminmin@msn.com>
2013-12-16 18:52:12 +0800

    Build: Search GLES SDK's include folder instead of SDK's folder.
    --HG--
    branch : 4.4

--- cmake/FindGLES.cmake
+++ cmake/FindGLES.cmake
@@ -2,34 +2,33 @@
 # Once done this will define
 #  
 #  KLAYGE_GLES_FOUND       - system has OpenGL ES
-#  KLAYGE_GLES_SDK_PATH    - Path to the OpenGL ES SDK
+#  KLAYGE_GLES_INCLUDE_DIR - Directory to the OpenGL ES SDK's include
 #   
 
-IF(NOT KLAYGE_GLES_SDK_PATH)
+IF(NOT KLAYGE_GLES_INCLUDE_DIR)
 	IF(WIN32)
-		FIND_PATH(KLAYGE_GLES_SDK_PATH include/KHR/khrplatform.h
+		FIND_PATH(KLAYGE_GLES_INCLUDE_DIR KHR/khrplatform.h
 			PATHS
-				"$ENV{ProgramFiles}/NVIDIA Corporation/win_x86_es2emu"
-				"$ENV{ProgramFiles(x86)}/NVIDIA Corporation/win_x86_es2emu"
-				"$ENV{SystemDrive}/Imagination/PowerVR/GraphicsSDK/SDK_*/Builds"
-				"$ENV{ProgramFiles}/ARM/Mali Developer Tools/Mali OpenGL ES Emulator*"
-				"$ENV{ProgramFiles(x86)}/ARM/Mali Developer Tools/Mali OpenGL ES Emulator*"
+				"$ENV{ProgramFiles}/NVIDIA Corporation/win_x86_es2emu/include"
+				"$ENV{ProgramFiles(x86)}/NVIDIA Corporation/win_x86_es2emu/include"
+				"$ENV{SystemDrive}/Imagination/PowerVR/GraphicsSDK/SDK_*/Builds/include"
+				"$ENV{ProgramFiles}/ARM/Mali Developer Tools/Mali OpenGL ES Emulator*/include"
+				"$ENV{ProgramFiles(x86)}/ARM/Mali Developer Tools/Mali OpenGL ES Emulator*/include"
 			)
-	ENDIF()
-	IF(UNIX)
-		FIND_PATH(KLAYGE_GLES_SDK_PATH include/KHR/khrplatform.h
+	ELSEIF(UNIX)
+		FIND_PATH(KLAYGE_GLES_INCLUDE_DIR KHR/khrplatform.h
 			PATHS
-				/usr/local
-				/usr
-				/usr/local/X11R6
-				/usr/X11R6
+				/usr/local/include
+				/usr/include
+				/usr/local/X11R6/include
+				/usr/X11R6/include
 		)
 	ENDIF()
 ENDIF()
 
-IF(KLAYGE_GLES_SDK_PATH)
+IF(KLAYGE_GLES_INCLUDE_DIR)
 	SET(KLAYGE_GLES_FOUND TRUE)
-	MESSAGE(STATUS "Found GLES SDK: ${KLAYGE_GLES_SDK_PATH}")
+	MESSAGE(STATUS "Found GLES SDK's include: ${KLAYGE_GLES_INCLUDE_DIR}")
 ELSE()
 	MESSAGE(STATUS "Could NOT find GLES SDK.")
 ENDIF()
