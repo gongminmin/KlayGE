461cb8412e24068385a5858779597e357ab56ed3
Minmin Gong <gongminmin@msn.com>
2014-06-05 18:48:08 +0800

    DXBC2GLSL: Add headers to all files. KlayGE: Core: Version string to 4.5.0.

--- DXBC2GLSL/Src/ShaderDefs.cpp
+++ DXBC2GLSL/Src/ShaderDefs.cpp
@@ -1,11 +1,32 @@
-//--------------------------------------------------------------------
-//this converter can convert HLSL5 to GLSL 2.1 3.x 4.x
-//for now, only support HLSL5->GLSL4.3
-//log:
-//dcl_immediateConstantBuffer unfound in disassembler.
-//sampleinfo samplepos hasn't corresponding functions in GLSL
-//ddiv dfma drcp in DX11.1 unfound in disassembler.
-//--------------------------------------------------------------------
+/**
+ * @file ShaderDefs.cpp
+ * @author Shenghua Lin, Minmin Gong
+ *
+ * @section DESCRIPTION
+ *
+ * This source file is part of KlayGE
+ * For the latest info, see http://www.klayge.org
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * You may alternatively use this source under the terms of
+ * the KlayGE Proprietary License (KPL). You can obtained such a license
+ * from http://www.klayge.org/licensing/.
+ */
 
 #include <DXBC2GLSL/ShaderDefs.hpp>
 #include <DXBC2GLSL/Utils.hpp>
