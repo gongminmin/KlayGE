3c8cc0d93316e714b1cdc9beeac2ec11a25870ea
Minmin Gong <gongminmin@msn.com>
2014-08-10 20:04:48 +0800

    Build: Move copy_to_dst to deploy_KlayGE.py.

--- deploy_KlayGE.py
+++ deploy_KlayGE.py
@@ -5,6 +5,11 @@ from __future__ import print_function
 import os, sys
 from blib_util import *
 
+def copy_to_dst(src_name, dst_dir):
+	print("Copy %s to %s" % (src_name, dst_dir))
+	import shutil
+	shutil.copy2(src_name, dst_dir)
+
 def deploy_KlayGE(target_dir, build_info, compiler_arch):
 	import glob
 
