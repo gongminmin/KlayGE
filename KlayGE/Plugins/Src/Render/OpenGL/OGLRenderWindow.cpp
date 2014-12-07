123da79fce356cf63d82b9bc341585843f3d1a9e
Minmin Gong <gongminmin@msn.com>
2014-12-07 00:06:04 -0800

    glloader & KlayGE: Fix ARB_framebuffer_object on old drivers.

--- KlayGE/Plugins/Src/Render/OpenGL/OGLRenderWindow.cpp
+++ KlayGE/Plugins/Src/Render/OpenGL/OGLRenderWindow.cpp
@@ -333,7 +333,11 @@ namespace KlayGE
 		uint32_t sample_count = settings.sample_count;
 #endif
 
-		if ((!glloader_GL_VERSION_2_1() || !glloader_GL_ARB_framebuffer_object()) && !glloader_GL_VERSION_3_0())
+		if (!glloader_GL_VERSION_3_0()
+			&& (!glloader_GL_VERSION_2_1()
+				|| !(glloader_GL_ARB_framebuffer_object()
+					|| (glloader_GL_EXT_framebuffer_object()
+						&& glloader_GL_EXT_framebuffer_blit()))))
 		{
 			THR(errc::function_not_supported);
 		}
