f1ee8bf16ab6e7a4ebe1bc8c4dd538bc00b9d0ed
Minmin Gong <gongminmin@msn.com>
2014-12-07 21:57:36 -0800

    Build: Use FindPythonInterp to locate python exe's path.

--- build_glloader.py
+++ build_glloader.py
@@ -6,10 +6,9 @@ import sys
 from blib_util import *
 
 def build_glloader(build_info):
-	additional_options = '-D PYTHON_EXE:STRING="%s"' % sys.executable
 	for compiler_info in build_info.compilers:
 		if not compiler_info.is_windows_runtime:
-			build_a_project("glloader", "glloader", build_info, compiler_info, True, additional_options)
+			build_a_project("glloader", "glloader", build_info, compiler_info, True)
 
 if __name__ == "__main__":
 	cfg = cfg_from_argv(sys.argv)
