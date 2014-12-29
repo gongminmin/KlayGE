2e1fb7872543f148c81680be356070a4f9ca732c
Minmin Gong <gongminmin@msn.com>
2014-12-28 23:14:40 -0800

    KlayGE: OpenGL: Add interface to D3DCompilerWrapper. (ticket #253)

--- KlayGE/Plugins/Src/Render/OpenGL/OGLShaderObject.cpp
+++ KlayGE/Plugins/Src/Render/OpenGL/OGLShaderObject.cpp
@@ -34,6 +34,7 @@
 #include <string>
 #include <algorithm>
 #include <sstream>
+#include <fstream>
 #include <cstring>
 #include <boost/assert.hpp>
 #ifdef KLAYGE_COMPILER_MSVC
@@ -57,18 +58,57 @@
 
 #if USE_DXBC2GLSL
 #include <DXBC2GLSL/DXBC2GLSL.hpp>
-#if defined(KLAYGE_COMPILER_GCC) || defined(KLAYGE_COMPILER_CLANG)
-	#define __in
-	#define __in_ecount(size)
-	#define __out
-	#define __out_ecount(size)
-	#define __in_bcount(size)
-	#define __in_opt
-	#define __in_ecount_opt(size)
-	#define __out_opt
-	#define __in_xcount_opt(size) 
+
+#ifdef KLAYGE_PLATFORM_WINDOWS
+#define CALL_D3DCOMPILER_DIRECTLY
 #endif
+
+#ifdef CALL_D3DCOMPILER_DIRECTLY
+
+#if defined(KLAYGE_COMPILER_GCC) || defined(KLAYGE_COMPILER_CLANG)
+#define __in
+#define __in_ecount(size)
+#define __out
+#define __out_ecount(size)
+#define __in_bcount(size)
+#define __in_opt
+#define __in_ecount_opt(size)
+#define __out_opt
+#define __in_xcount_opt(size) 
+#endif
+
 #include <D3DCompiler.h>
+#else
+// http://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx
+typedef char const * LPCSTR;
+typedef long HRESULT;
+
+#define D3DCOMPILE_DEBUG                            0x00000001
+#define D3DCOMPILE_SKIP_VALIDATION                  0x00000002
+#define D3DCOMPILE_SKIP_OPTIMIZATION                0x00000004
+#define D3DCOMPILE_PACK_MATRIX_ROW_MAJOR            0x00000008
+#define D3DCOMPILE_PACK_MATRIX_COLUMN_MAJOR         0x00000010
+#define D3DCOMPILE_PARTIAL_PRECISION                0x00000020
+#define D3DCOMPILE_FORCE_VS_SOFTWARE_NO_OPT         0x00000040
+#define D3DCOMPILE_FORCE_PS_SOFTWARE_NO_OPT         0x00000080
+#define D3DCOMPILE_NO_PRESHADER                     0x00000100
+#define D3DCOMPILE_AVOID_FLOW_CONTROL               0x00000200
+#define D3DCOMPILE_PREFER_FLOW_CONTROL              0x00000400
+#define D3DCOMPILE_ENABLE_STRICTNESS                0x00000800
+#define D3DCOMPILE_ENABLE_BACKWARDS_COMPATIBILITY   0x00001000
+#define D3DCOMPILE_IEEE_STRICTNESS                  0x00002000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL0              0x00004000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL1              0x00000000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL2              0x0000c000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL3              0x00008000
+#define D3DCOMPILE_WARNINGS_ARE_ERRORS              0x00040000
+
+typedef struct _D3D_SHADER_MACRO
+{
+	LPCSTR Name;
+	LPCSTR Definition;
+} D3D_SHADER_MACRO, *LPD3D_SHADER_MACRO;
+#endif
 #else
 #include <Cg/cg.h>
 #endif
@@ -98,7 +138,9 @@ namespace
 	public:
 		~DXBC2GLSLIniter()
 		{
+#ifdef CALL_D3DCOMPILER_DIRECTLY
 			::FreeLibrary(mod_d3dcompiler_);
+#endif
 		}
 
 		static DXBC2GLSLIniter& Instance()
@@ -107,12 +149,122 @@ namespace
 			return initer;
 		}
 
-		HRESULT D3DCompile(LPCVOID pSrcData, SIZE_T SrcDataSize, LPCSTR pSourceName,
-			D3D_SHADER_MACRO const * pDefines, ID3DInclude* pInclude, LPCSTR pEntrypoint,
-			LPCSTR pTarget, UINT Flags1, UINT Flags2, ID3DBlob** ppCode, ID3DBlob** ppErrorMsgs) const
+		HRESULT D3DCompile(std::string const & src_data,
+			D3D_SHADER_MACRO const * defines, std::string const & entry_point,
+			std::string const & target, uint32_t flags1, uint32_t flags2,
+			std::vector<uint8_t>& code, std::string& error_msgs) const
 		{
-			return DynamicD3DCompile_(pSrcData, SrcDataSize, pSourceName, pDefines, pInclude, pEntrypoint,
-				pTarget, Flags1, Flags2, ppCode, ppErrorMsgs);
+#ifdef CALL_D3DCOMPILER_DIRECTLY
+			ID3DBlob* code_blob = nullptr;
+			ID3DBlob* error_msgs_blob = nullptr;
+			HRESULT hr = DynamicD3DCompile_(src_data.c_str(), static_cast<UINT>(src_data.size()),
+				nullptr, defines, nullptr, entry_point.c_str(),
+				target.c_str(), flags1, flags2, &code_blob, &error_msgs_blob);
+			if (code_blob)
+			{
+				uint8_t const * p = static_cast<uint8_t const *>(code_blob->GetBufferPointer());
+				code.assign(p, p + code_blob->GetBufferSize());
+				code_blob->Release();
+			}
+			else
+			{
+				code.clear();
+			}
+			if (error_msgs_blob)
+			{
+				char const * p = static_cast<char const *>(error_msgs_blob->GetBufferPointer());
+				error_msgs.assign(p, p + error_msgs_blob->GetBufferSize());
+				error_msgs_blob->Release();
+			}
+			else
+			{
+				error_msgs.clear();
+			}
+			return hr;
+#else
+			std::string mark = boost::lexical_cast<std::string>(static_cast<void const *>(src_data.c_str()));
+			std::string compile_input_file = entry_point + mark + "Input.tmp";
+			std::string compile_output_file = entry_point + mark + "Output.tmp";
+
+			uint32_t buffer_size;
+			
+			std::ofstream ofs(compile_input_file.c_str(), std::ios_base::binary);
+
+			buffer_size = static_cast<uint32_t>(src_data.size());
+			ofs.write(reinterpret_cast<char const *>(&buffer_size), sizeof(buffer_size));
+			ofs.write(src_data.c_str(), buffer_size);
+		
+			uint32_t idx = 0;
+			while ((defines[idx].Definition != nullptr) && (defines[idx].Name != nullptr))
+			{
+				++ idx;
+			}
+
+			ofs.write(reinterpret_cast<char const *>(&idx), sizeof(idx));
+
+			idx = 0;
+			while ((defines[idx].Definition != nullptr) && (defines[idx].Name != nullptr))
+			{
+				ofs << defines[idx].Name << std::endl;
+				ofs << defines[idx].Definition << std::endl;
+				++ idx;
+			}
+
+			ofs.close();
+			
+			std::stringstream ss;
+			std::string d3dcompiler_wrapper_name = "D3DCompilerWrapper";
+#ifdef KLAYGE_DEBUG
+			d3dcompiler_wrapper_name += "_d";
+#endif
+#ifdef KLAYGE_PLATFORM_WINDOWS
+			ss << d3dcompiler_wrapper_name << ".exe";
+#else
+			ss << "/usr/local/bin/wine " << d3dcompiler_wrapper_name << ".exe.so";
+#endif
+			ss << " " << compile_input_file;
+			ss << " " << entry_point << " " << target;
+			ss << " " << flags1 << " " << flags2;
+			ss << " " << compile_output_file;
+			if (system(ss.str().c_str()) != 0)
+			{
+				return -1;
+			}
+
+			std::ifstream ifs(compile_output_file.c_str(), std::ios_base::binary);
+
+			HRESULT hr;
+			ifs.read(reinterpret_cast<char*>(&hr), sizeof(hr));
+
+			ifs.read(reinterpret_cast<char*>(&buffer_size), sizeof(buffer_size));
+			if (buffer_size > 0)
+			{
+				code.resize(buffer_size);
+				ifs.read(reinterpret_cast<char*>(&code[0]), buffer_size);
+			}
+			else
+			{
+				code.clear();
+			}
+			
+			ifs.read(reinterpret_cast<char*>(&buffer_size), sizeof(buffer_size));
+			if (buffer_size > 0)
+			{
+				error_msgs.resize(buffer_size);
+				ifs.read(&error_msgs[0], buffer_size);
+			}
+			else
+			{
+				error_msgs.clear();
+			}
+
+			ifs.close();
+
+			remove(compile_input_file.c_str());
+			remove(compile_output_file.c_str());
+			
+			return hr;
+#endif
 		}
 
 		GLSLVersion GLSLVer() const
@@ -123,12 +275,13 @@ namespace
 	private:
 		DXBC2GLSLIniter()
 		{
+#ifdef CALL_D3DCOMPILER_DIRECTLY
 			mod_d3dcompiler_ = ::LoadLibraryEx(TEXT("d3dcompiler_47.dll"), nullptr, 0);
 #ifdef KLAYGE_COMPILER_MSVC
 			__assume(mod_d3dcompiler_ != nullptr);
 #endif
 			DynamicD3DCompile_ = reinterpret_cast<D3DCompileFunc>(::GetProcAddress(mod_d3dcompiler_, "D3DCompile"));
-
+#endif
 			if (glloader_GL_VERSION_4_4())
 			{
 				gsv_ = GSV_440;
@@ -172,12 +325,14 @@ namespace
 		}
 
 	private:
+#ifdef CALL_D3DCOMPILER_DIRECTLY
 		typedef HRESULT(WINAPI *D3DCompileFunc)(LPCVOID pSrcData, SIZE_T SrcDataSize, LPCSTR pSourceName,
 			D3D_SHADER_MACRO const * pDefines, ID3DInclude* pInclude, LPCSTR pEntrypoint,
 			LPCSTR pTarget, UINT Flags1, UINT Flags2, ID3DBlob** ppCode, ID3DBlob** ppErrorMsgs);
 
 		HMODULE mod_d3dcompiler_;
 		D3DCompileFunc DynamicD3DCompile_;
+#endif
 		GLSLVersion gsv_;
 	};
 #else
@@ -2410,10 +2565,10 @@ namespace KlayGE
 				break;
 			}
 
-			ID3DBlob* code = nullptr;
+			std::vector<uint8_t> code;
 			if (is_shader_validate_[type])
 			{
-				ID3DBlob* err_msg;
+				std::string err_msg;
 				std::vector<D3D_SHADER_MACRO> macros;
 				{
 					D3D_SHADER_MACRO macro_d3d11 = { "KLAYGE_DXBC2GLSL", "1" };
@@ -2517,17 +2672,16 @@ namespace KlayGE
 				}
 				uint32_t flags = D3DCOMPILE_PREFER_FLOW_CONTROL | D3DCOMPILE_SKIP_OPTIMIZATION;
 
-				DXBC2GLSLIniter::Instance().D3DCompile(hlsl_shader_text.c_str(),
-					static_cast<UINT>(hlsl_shader_text.size()), nullptr, &macros[0],
-					nullptr, sd.func_name.c_str(), shader_profile.c_str(),
-					flags, 0, &code, &err_msg);
-				if (err_msg != nullptr)
+				DXBC2GLSLIniter::Instance().D3DCompile(hlsl_shader_text, &macros[0],
+					sd.func_name, shader_profile,
+					flags, 0, code, err_msg);
+				if (!err_msg.empty())
 				{
 					LogError("Error when compiling %s:", sd.func_name.c_str());
 
 					std::map<int, std::vector<std::string> > err_lines;
 					{
-						std::istringstream err_iss(static_cast<char*>(err_msg->GetBufferPointer()));
+						std::istringstream err_iss(err_msg);
 						std::string err_str;
 						while (err_iss)
 						{
@@ -2602,11 +2756,9 @@ namespace KlayGE
 							LogError(msg.c_str());
 						}
 					}
-
-					err_msg->Release();
 				}
 
-				if (code)
+				if (!code.empty())
 				{
 					try
 					{
@@ -2614,7 +2766,7 @@ namespace KlayGE
 						DXBC2GLSL::DXBC2GLSL dxbc2glsl;
 						uint32_t rules = DXBC2GLSL::DXBC2GLSL::DefaultRules(gsv);
 						rules &= ~GSR_UniformBlockBinding;
-						dxbc2glsl.FeedDXBC(code->GetBufferPointer(), has_gs, gsv, rules);
+						dxbc2glsl.FeedDXBC(&code[0], has_gs, gsv, rules);
 						(*glsl_srcs_)[type] = MakeSharedPtr<std::string>(dxbc2glsl.GLSLString());
 						(*pnames_)[type] = MakeSharedPtr<std::vector<std::string> >();
 						(*glsl_res_names_)[type] = MakeSharedPtr<std::vector<std::string> >();
@@ -2815,8 +2967,6 @@ namespace KlayGE
 						LogError(ex.what());
 						LogError("Please send this information and your shader to webmaster at klayge.org. We'll fix this ASAP.");
 					}
-
-					code->Release();
 				}
 				else
 				{
--- KlayGE/Tools/src/D3DCompilerWrapper/D3DCompilerWrapper.cpp
+++ KlayGE/Tools/src/D3DCompilerWrapper/D3DCompilerWrapper.cpp
@@ -53,39 +53,36 @@ int main(int argc, char* argv[])
 		printf("GetProcAddress fail\n");
 		return -1;
 	}
-	if (argc < 9)
+	if (argc < 7)
 	{
 #ifdef WIN32
-		printf("D3DCompilerWrapper.exe hlsl_file defines_file entry_point target flags1 flags2 dxbc_file error_file");
+		printf("D3DCompilerWrapper.exe input_file entry_point target flags1 flags2 output_file");
 #else
-		printf("wine D3DCompilerWrapper.so hlsl_file defines_file entry_point target flags1 flags2 dxbc_file error_file");
+		printf("wine D3DCompilerWrapper.so input_file defines_file entry_point target flags1 flags2 output_file");
 #endif
 		return -1;
 	}
 
-	char const * hlsl_file = argv[1];
-	char const * defines_file = argv[2];
-	char const * entry_point = argv[3];
-	char const * target = argv[4];
-	int flags1 = atoi(argv[5]);
-	int flags2 = atoi(argv[6]);
-	char const * dxbc_file = argv[7];
-	char const * error_file = argv[8];
+	char const * input_file = argv[1];
+	char const * entry_point = argv[2];
+	char const * target = argv[3];
+	int flags1 = atoi(argv[4]);
+	int flags2 = atoi(argv[5]);
+	char const * output_file = argv[6];
 
-	FILE* fp = fopen(hlsl_file, "r");
-	fseek(fp, 0, SEEK_END);
-	long buffer_size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	char* buffer = new char[buffer_size + 1];
-	fread(buffer, sizeof(char), buffer_size, fp);
-	fclose(fp);
-	buffer[buffer_size] = 0;
+	FILE* fp = fopen(input_file, "rb");
+	int hlsl_size;
+	fread(&hlsl_size, sizeof(hlsl_size), 1, fp);
+	char* hlsl = new char[hlsl_size + 1];
+	fread(hlsl, sizeof(char), hlsl_size, fp);
+	hlsl[hlsl_size] = 0;
 
-	D3D_SHADER_MACRO* macros = new D3D_SHADER_MACRO[128];
+	int num_macros;
+	fread(&num_macros, sizeof(num_macros), 1, fp);
+	D3D_SHADER_MACRO* macros = new D3D_SHADER_MACRO[num_macros + 1];
 	char line_name[1024];
 	char line_definition[1024];
 	int idx = 0;
-	fp = fopen(defines_file, "r");
 	while (fgets(line_name, 1024, fp) && fgets(line_definition, 1024, fp))
 	{
 		char* t1 = new char[strlen(line_name) + 1];
@@ -110,32 +107,45 @@ int main(int argc, char* argv[])
 
 	ID3DBlob* code = NULL;
 	ID3DBlob* err_msg = NULL;
-	HRESULT hr = DynamicD3DCompile(buffer, buffer_size, NULL, macros, NULL, entry_point,
+	HRESULT hr = DynamicD3DCompile(hlsl, hlsl_size, NULL, macros, NULL, entry_point,
 		target, flags1, flags2, &code, &err_msg);
-	fp = fopen(error_file, "w");
 	if (FAILED(hr))
 	{
 		printf("Compiling error: 0x%x", hr);
 	}
-	if (err_msg != NULL)
-	{
-		fwrite(err_msg->GetBufferPointer(), sizeof(char), err_msg->GetBufferSize(), fp);
-	}
-	fclose(fp);
-	fp = fopen(dxbc_file, "wb");
+	fp = fopen(output_file, "wb");
+	fwrite(&hr, sizeof(hr), 1, fp);
 	if (code != NULL)
 	{
-		fwrite(code->GetBufferPointer(), sizeof(char), code->GetBufferSize(), fp);
+		int const size = static_cast<int>(code->GetBufferSize());
+		fwrite(&size, sizeof(size), 1, fp);
+		fwrite(code->GetBufferPointer(), sizeof(char), size, fp);
+	}
+	else
+	{
+		int const size = 0;
+		fwrite(&size, sizeof(size), 1, fp);
+	}
+	if (err_msg != NULL)
+	{
+		int const size = static_cast<int>(err_msg->GetBufferSize());
+		fwrite(&size, sizeof(size), 1, fp);
+		fwrite(err_msg->GetBufferPointer(), sizeof(char), err_msg->GetBufferSize(), fp);
+	}
+	else
+	{
+		int const size = 0;
+		fwrite(&size, sizeof(size), 1, fp);
 	}
 	fclose(fp);
 
-	for (int i = 0; i < idx; ++ i)
+	for (int i = 0; i < num_macros; ++ i)
 	{
 		delete[] macros[i].Name;
 		delete[] macros[i].Definition;
 	}
 	delete[] macros;
-	delete[] buffer;
+	delete[] hlsl;
 
 	FreeLibrary(mod_d3dcompiler);
 
