cc0d634a4a100e575c9cf3f14fe51060ca64c922
Minmin Gong <gongminmin@msn.com>
2015-01-03 18:23:42 -0800

    KlayGE: Tests: Adding texture encode decode testings on .dds textures.

--- KlayGE/Tests/src/EncodeDecodeTex.cpp
+++ KlayGE/Tests/src/EncodeDecodeTex.cpp
@@ -2,6 +2,8 @@
 #include <KlayGE/TexCompressionBC.hpp>
 #include <KlayGE/TexCompressionETC.hpp>
 #include <KlayGE/Texture.hpp>
+#include <KlayGE/ResLoader.hpp>
+#include <KFL/Half.hpp>
 
 #include <boost/assert.hpp>
 
@@ -15,24 +17,13 @@
 using namespace std;
 using namespace KlayGE;
 
-uint32_t lena_xrgb[16] = 
+void TestEncodeDecodeTex(std::string const & input_name, std::string const & tc_name,
+		ElementFormat bc_fmt, float threshold)
 {
-	0xFFC55D5A, 0xFFCC6E6D, 0xFFCD7775, 0xFFAB595F,
-	0xFFBF5858, 0xFFAF5B6A, 0xFFC98F8F, 0xFFB06B71,
-	0xFFA75162, 0xFF92445E, 0xFFB35866, 0xFFCD907F,
-	0xFF914C60, 0xFF8B3D5A, 0xFFD2817B, 0xFFAE6363
-};
+	std::vector<uint8_t> input_argb;
+	std::vector<uint8_t> bc_blocks;
+	uint32_t width, height;
 
-uint32_t leaf_argb[16] = 
-{
-	0xFE5E854A, 0xF65A8A44, 0xD4548542, 0x905D7F4C,
-	0xFF648550, 0xFA5C854A, 0xE3588745, 0xA8587F47,
-	0xFF5E854C, 0xFC60834E, 0xED578449, 0xBE558344,
-	0xFF62874F, 0xFE60844E, 0xF55E844E, 0xD15B8849
-};
-
-void TestEncodeDecodeTex(uint32_t const * argb, ElementFormat bc_fmt, float threshold)
-{
 	TexCompressionPtr codec;
 	switch (bc_fmt)
 	{
@@ -48,6 +39,18 @@ void TestEncodeDecodeTex(uint32_t const * argb, ElementFormat bc_fmt, float thre
 		codec = MakeSharedPtr<TexCompressionBC3>();
 		break;
 
+	case EF_BC6:
+		codec = MakeSharedPtr<TexCompressionBC6U>();
+		break;
+
+	case EF_SIGNED_BC6:
+		codec = MakeSharedPtr<TexCompressionBC6S>();
+		break;
+
+	case EF_BC7:
+		codec = MakeSharedPtr<TexCompressionBC7>();
+		break;
+
 	case EF_ETC1:
 		codec = MakeSharedPtr<TexCompressionETC1>();
 		break;
@@ -59,87 +62,231 @@ void TestEncodeDecodeTex(uint32_t const * argb, ElementFormat bc_fmt, float thre
 
 	ElementFormat const decoded_fmt = codec->DecodedFormat();
 	uint32_t const pixel_size = NumFormatBytes(decoded_fmt);
-	BOOST_CHECK(4 == pixel_size);
+
+	{
+		Texture::TextureType type;
+		uint32_t depth, num_mipmaps, array_size;
+		ElementFormat format;
+		std::vector<ElementInitData> init_data;
+		std::vector<uint8_t> data_block;
+		LoadTexture(input_name, type, width, height, depth, num_mipmaps, array_size,
+			format, init_data, data_block);
+
+		BOOST_ASSERT(pixel_size == NumFormatBytes(format));
+
+		input_argb.resize(width * height * pixel_size);
+		array<uint8_t, 16> pixel;
+
+		uint8_t const * src = static_cast<uint8_t const *>(init_data[0].data);
+		uint32_t const pitch = init_data[0].row_pitch;
+		for (uint32_t y = 0; y < height; ++ y)
+		{
+			for (uint32_t x = 0; x < width; ++ x)
+			{
+				memcpy(&pixel[0], &src[x * pixel_size], pixel_size);
+				if (EF_BC1 == bc_fmt)
+				{
+					if (pixel[3] < 128)
+					{
+						pixel[0] = 0;
+						pixel[1] = 0;
+						pixel[2] = 0;
+						pixel[3] = 0;
+					}
+					else
+					{
+						pixel[3] = 255;
+					}
+				}
+
+				memcpy(&input_argb[(y * width + x) * pixel_size], &pixel[0], pixel_size);
+			}
+
+			src += pitch;
+		}
+	}
 
 	uint32_t const block_width = codec->BlockWidth();
 	uint32_t const block_height = codec->BlockWidth();
 	uint32_t const block_bytes = codec->BlockBytes();
-	std::vector<uint8_t> bc_blocks(block_bytes);
-	std::vector<uint8_t> input_argb(block_width * block_height * pixel_size);
-	memcpy(&input_argb[0], argb, input_argb.size());
-	if (EF_BC1 == bc_fmt)
+	bc_blocks.resize((width + block_width - 1) / block_width * (height + block_height - 1) / block_height * block_bytes);
+
+	if (tc_name.empty())
 	{
-		for (uint32_t i = 0; i < block_width * block_height; ++ i)
+		for (uint32_t y_base = 0; y_base < height; y_base += block_height)
 		{
-			if ((argb[i] >> 24) < 128)
+			for (uint32_t x_base = 0; x_base < width; x_base += block_width)
 			{
-				input_argb[i * 4 + 0] = 0;
-				input_argb[i * 4 + 1] = 0;
-				input_argb[i * 4 + 2] = 0;
-				input_argb[i * 4 + 3] = 0;
+				std::vector<uint8_t> uncompressed(block_width * block_height * pixel_size);
+				for (uint32_t y = 0; y < block_height; ++ y)
+				{
+					for (uint32_t x = 0; x < block_width; ++ x)
+					{
+						if ((x_base + x < width) && (y_base + y < height))
+						{
+							memcpy(&uncompressed[(y * block_width + x) * pixel_size],
+								&input_argb[((y_base + y) * width + (x_base + x)) * pixel_size], pixel_size);
+						}
+						else
+						{
+							memset(&uncompressed[(y * block_width + x) * pixel_size], 0, pixel_size);
+						}
+					}
+				}
+
+				uint32_t index = ((y_base / block_height) * ((width + block_width - 1) / block_width) + (x_base / block_width)) * block_bytes;
+				codec->EncodeBlock(&bc_blocks[index], &uncompressed[0], TCM_Balanced);
 			}
-			else
+		}
+	}
+	else
+	{
+		Texture::TextureType type;
+		uint32_t depth, num_mipmaps, array_size;
+		ElementFormat format;
+		std::vector<ElementInitData> init_data;
+		std::vector<uint8_t> data_block;
+		LoadTexture(tc_name, type, width, height, depth, num_mipmaps, array_size,
+			format, init_data, data_block);
+
+		uint8_t const * src = static_cast<uint8_t const *>(init_data[0].data);
+		uint32_t const pitch = init_data[0].row_pitch;
+		for (uint32_t y = 0; y < (height + block_height - 1) / block_height; ++ y)
+		{
+			memcpy(&bc_blocks[y * ((width + block_width - 1) / block_width * block_bytes)], src,
+				(width + block_width - 1) / block_width * block_bytes);
+			src += pitch;
+		}
+	}
+
+	std::vector<uint8_t> restored_argb(width * height * pixel_size);
+	for (uint32_t y_base = 0; y_base < height; y_base += block_height)
+	{
+		for (uint32_t x_base = 0; x_base < width; x_base += block_width)
+		{
+			uint32_t index = ((y_base / block_height) * ((width + block_width - 1) / block_width) + (x_base / block_width)) * block_bytes;
+
+			std::vector<uint8_t> argb_block(block_width * block_height * pixel_size);
+			codec->DecodeBlock(&argb_block[0], &bc_blocks[index]);
+			for (uint32_t y = 0; y < block_height; ++ y)
 			{
-				input_argb[i * 4 + 3] = 255;
+				for (uint32_t x = 0; x < block_width; ++ x)
+				{
+					if ((x_base + x < width) && (y_base + y < height))
+					{
+						memcpy(&restored_argb[((y_base + y) * width + (x_base + x)) * pixel_size],
+							&argb_block[(y * block_width + x) * pixel_size], pixel_size);
+					}
+				}
 			}
 		}
 	}
 
-	codec->EncodeBlock(&bc_blocks[0], &input_argb[0], TCM_Quality);
-
-	std::vector<uint8_t> restored_argb(block_width * block_height * pixel_size);
-	codec->DecodeBlock(&restored_argb[0], &bc_blocks[0]);
-
 	float mse = 0;
-	for (uint32_t i = 0; i < block_width * block_height; ++i)
+	if (EF_ABGR16F == decoded_fmt)
 	{
-		int const a0 = input_argb[i * 4 + 3];
-		int const r0 = input_argb[i * 4 + 2];
-		int const g0 = input_argb[i * 4 + 1];
-		int const b0 = input_argb[i * 4 + 0];
-		int const a1 = restored_argb[i * 4 + 3];
-		int const r1 = restored_argb[i * 4 + 2];
-		int const g1 = restored_argb[i * 4 + 1];
-		int const b1 = restored_argb[i * 4 + 0];
+		for (uint32_t y = 0; y < height; ++y)
+		{
+			for (uint32_t x = 0; x < width; ++x)
+			{
+				float const a0 = *reinterpret_cast<half const *>(&input_argb[((y * width + x) * 4 + 3) * 2]);
+				float const r0 = *reinterpret_cast<half const *>(&input_argb[((y * width + x) * 4 + 2) * 2]);
+				float const g0 = *reinterpret_cast<half const *>(&input_argb[((y * width + x) * 4 + 1) * 2]);
+				float const b0 = *reinterpret_cast<half const *>(&input_argb[((y * width + x) * 4 + 0) * 2]);
+				float const a1 = *reinterpret_cast<half const *>(&restored_argb[((y * width + x) * 4 + 3) * 2]);
+				float const r1 = *reinterpret_cast<half const *>(&restored_argb[((y * width + x) * 4 + 2) * 2]);
+				float const g1 = *reinterpret_cast<half const *>(&restored_argb[((y * width + x) * 4 + 1) * 2]);
+				float const b1 = *reinterpret_cast<half const *>(&restored_argb[((y * width + x) * 4 + 0) * 2]);
 
-		int diff_a = a0 - a1;
-		int diff_r = r0 - r1;
-		int diff_g = g0 - g1;
-		int diff_b = b0 - b1;
+				float diff_a = a0 - a1;
+				float diff_r = r0 - r1;
+				float diff_g = g0 - g1;
+				float diff_b = b0 - b1;
 
-		mse += diff_a * diff_a + diff_r * diff_r + diff_g * diff_g + diff_b * diff_b;
+				mse += (diff_a * diff_a + diff_r * diff_r + diff_g * diff_g + diff_b * diff_b);
+			}
+		}
+	}
+	else
+	{
+		for (uint32_t y = 0; y < height; ++y)
+		{
+			for (uint32_t x = 0; x < width; ++x)
+			{
+				float const a0 = input_argb[(y * width + x) * 4 + 3];
+				float const r0 = input_argb[(y * width + x) * 4 + 2];
+				float const g0 = input_argb[(y * width + x) * 4 + 1];
+				float const b0 = input_argb[(y * width + x) * 4 + 0];
+				float const a1 = restored_argb[(y * width + x) * 4 + 3];
+				float const r1 = restored_argb[(y * width + x) * 4 + 2];
+				float const g1 = restored_argb[(y * width + x) * 4 + 1];
+				float const b1 = restored_argb[(y * width + x) * 4 + 0];
+
+				float diff_a = a0 - a1;
+				float diff_r = r0 - r1;
+				float diff_g = g0 - g1;
+				float diff_b = b0 - b1;
+
+				mse += diff_a * diff_a + diff_r * diff_r + diff_g * diff_g + diff_b * diff_b;
+			}
+		}
 	}
 
-	mse = sqrt(mse / (block_width * block_height) / 4);
+	mse = sqrt(mse / (width * height) / 4);
 	BOOST_CHECK(mse < threshold);
 }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeBC1XRGB)
+BOOST_AUTO_TEST_CASE(Init)
 {
-	TestEncodeDecodeTex(lena_xrgb, EF_BC1, 8);
+	ResLoader::Instance().AddPath("../../Tests/media");
 }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeBC2XRGB)
+BOOST_AUTO_TEST_CASE(DecodeBC1)
 {
-	TestEncodeDecodeTex(lena_xrgb, EF_BC2, 11);
+	TestEncodeDecodeTex("Lenna.dds", "Lenna_bc1.dds", EF_BC1, 5);
 }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeBC2ARGB)
+BOOST_AUTO_TEST_CASE(DecodeBC2)
 {
-	TestEncodeDecodeTex(leaf_argb, EF_BC2, 6);
+	TestEncodeDecodeTex("leaf_v3_green_tex.dds", "leaf_v3_green_tex_bc2.dds", EF_BC2, 10);
 }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeBC3XRGB)
+BOOST_AUTO_TEST_CASE(DecodeBC3)
 {
-	TestEncodeDecodeTex(lena_xrgb, EF_BC3, 8);
+	TestEncodeDecodeTex("leaf_v3_green_tex.dds", "leaf_v3_green_tex_bc3.dds", EF_BC3, 9);
 }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeBC3ARGB)
+BOOST_AUTO_TEST_CASE(DecodeBC7XRGB)
 {
-	TestEncodeDecodeTex(leaf_argb, EF_BC3, 3);
+	TestEncodeDecodeTex("Lenna.dds", "Lenna_bc7.dds", EF_BC7, 3);
 }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeETC1XRGB)
+BOOST_AUTO_TEST_CASE(DecodeBC7ARGB)
 {
-	TestEncodeDecodeTex(lena_xrgb, EF_ETC1, 8);
+	TestEncodeDecodeTex("leaf_v3_green_tex.dds", "leaf_v3_green_tex_bc7.dds", EF_BC7, 5);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeDecodeBC1)
+{
+	TestEncodeDecodeTex("Lenna.dds", "", EF_BC1, 5);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeDecodeBC2)
+{
+	TestEncodeDecodeTex("leaf_v3_green_tex.dds", "", EF_BC2, 10);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeDecodeBC3)
+{
+	TestEncodeDecodeTex("leaf_v3_green_tex.dds", "", EF_BC3, 9);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeDecodeETC1)
+{
+	TestEncodeDecodeTex("Lenna.dds", "", EF_ETC1, 5);
+}
+
+BOOST_AUTO_TEST_CASE(Deinit)
+{
+	Context::Destroy();
 }
