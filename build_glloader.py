0d06030735361d0f8011acc495718587a028f89f
Kanglai Qian <qiankanglai@gmail.com>
2015-04-01 22:06:18 -0700

    Build: KlayGE: Link dynamic libraries under bin directory.

--- build_glloader.py
+++ build_glloader.py
@@ -8,7 +8,7 @@ from blib_util import *
 def build_glloader(build_info):
 	for compiler_info in build_info.compilers:
 		if not compiler_info.is_windows_runtime:
-			build_a_project("glloader", "glloader", build_info, compiler_info, True)
+			build_a_project("glloader", "glloader", build_info, compiler_info, compiler_info.is_windows)
 
 if __name__ == "__main__":
 	cfg = cfg_from_argv(sys.argv)
--- build_kfont.py
+++ build_kfont.py
@@ -7,7 +7,7 @@ from blib_util import *
 
 def build_kfont(build_info):
 	for compiler_info in build_info.compilers:
-		build_a_project("kfont", "kfont", build_info, compiler_info, True)
+		build_a_project("kfont", "kfont", build_info, compiler_info, compiler_info.is_windows)
 
 if __name__ == "__main__":
 	cfg = cfg_from_argv(sys.argv)
