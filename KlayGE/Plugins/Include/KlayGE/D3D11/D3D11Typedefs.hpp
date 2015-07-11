db1722a2f38d670f13dbd00dec7f8accfe7ca756
Minmin Gong <gongminmin@msn.com>
2015-07-11 13:12:37 -0700

    KFL: Add a wrapper for constexpr. (ticket #271)

--- KlayGE/Plugins/Include/KlayGE/D3D11/D3D11Typedefs.hpp
+++ KlayGE/Plugins/Include/KlayGE/D3D11/D3D11Typedefs.hpp
@@ -47,30 +47,30 @@ namespace KlayGE
 #endif
 	typedef std::shared_ptr<IDXGISwapChain>				IDXGISwapChainPtr;
 	typedef std::shared_ptr<ID3D11Device>				ID3D11DevicePtr;
-	typedef std::shared_ptr<ID3D11DeviceContext>			ID3D11DeviceContextPtr;
+	typedef std::shared_ptr<ID3D11DeviceContext>		ID3D11DeviceContextPtr;
 #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
-	typedef std::shared_ptr<IDXGISwapChain1>				IDXGISwapChain1Ptr;
+	typedef std::shared_ptr<IDXGISwapChain1>			IDXGISwapChain1Ptr;
 	typedef std::shared_ptr<ID3D11Device1>				ID3D11Device1Ptr;
 	typedef std::shared_ptr<ID3D11DeviceContext1>		ID3D11DeviceContext1Ptr;
 #endif
 #if (_WIN32_WINNT >= _WIN32_WINNT_WINBLUE)
-	typedef std::shared_ptr<IDXGISwapChain2>				IDXGISwapChain2Ptr;
+	typedef std::shared_ptr<IDXGISwapChain2>			IDXGISwapChain2Ptr;
 	typedef std::shared_ptr<ID3D11Device2>				ID3D11Device2Ptr;
 	typedef std::shared_ptr<ID3D11DeviceContext2>		ID3D11DeviceContext2Ptr;
 #endif
 	typedef std::shared_ptr<ID3D11Resource>				ID3D11ResourcePtr;
-	typedef std::shared_ptr<ID3D11Texture1D>				ID3D11Texture1DPtr;
-	typedef std::shared_ptr<ID3D11Texture2D>				ID3D11Texture2DPtr;
-	typedef std::shared_ptr<ID3D11Texture3D>				ID3D11Texture3DPtr;
-	typedef std::shared_ptr<ID3D11Texture2D>				ID3D11TextureCubePtr;
+	typedef std::shared_ptr<ID3D11Texture1D>			ID3D11Texture1DPtr;
+	typedef std::shared_ptr<ID3D11Texture2D>			ID3D11Texture2DPtr;
+	typedef std::shared_ptr<ID3D11Texture3D>			ID3D11Texture3DPtr;
+	typedef std::shared_ptr<ID3D11Texture2D>			ID3D11TextureCubePtr;
 	typedef std::shared_ptr<ID3D11Buffer>				ID3D11BufferPtr;
 	typedef std::shared_ptr<ID3D11InputLayout>			ID3D11InputLayoutPtr;
-	typedef std::shared_ptr<ID3D11Query>					ID3D11QueryPtr;
-	typedef std::shared_ptr<ID3D11Predicate>				ID3D11PredicatePtr;
+	typedef std::shared_ptr<ID3D11Query>				ID3D11QueryPtr;
+	typedef std::shared_ptr<ID3D11Predicate>			ID3D11PredicatePtr;
 	typedef std::shared_ptr<ID3D11VertexShader>			ID3D11VertexShaderPtr;
 	typedef std::shared_ptr<ID3D11PixelShader>			ID3D11PixelShaderPtr;
 	typedef std::shared_ptr<ID3D11GeometryShader>		ID3D11GeometryShaderPtr;
-	typedef std::shared_ptr<ID3D11ComputeShader>			ID3D11ComputeShaderPtr;
+	typedef std::shared_ptr<ID3D11ComputeShader>		ID3D11ComputeShaderPtr;
 	typedef std::shared_ptr<ID3D11HullShader>			ID3D11HullShaderPtr;
 	typedef std::shared_ptr<ID3D11DomainShader>			ID3D11DomainShaderPtr;
 	typedef std::shared_ptr<ID3D11RenderTargetView>		ID3D11RenderTargetViewPtr;
@@ -80,7 +80,7 @@ namespace KlayGE
 #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
 	typedef std::shared_ptr<ID3D11RasterizerState1>		ID3D11RasterizerState1Ptr;
 #endif
-	typedef std::shared_ptr<ID3D11DepthStencilState>		ID3D11DepthStencilStatePtr;
+	typedef std::shared_ptr<ID3D11DepthStencilState>	ID3D11DepthStencilStatePtr;
 	typedef std::shared_ptr<ID3D11BlendState>			ID3D11BlendStatePtr;
 #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
 	typedef std::shared_ptr<ID3D11BlendState1>			ID3D11BlendState1Ptr;
