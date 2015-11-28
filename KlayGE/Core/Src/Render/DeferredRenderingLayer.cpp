e224cc23e91393f86f9cd883375c764d4a6d6213
Minmin Gong <gongminmin@msn.com>
2015-11-28 10:33:45 -0800

    KlayGE: DR: Keep unfiltered shadow map only when translucency is on, reported by Kanglai Qian. (ticket #259)

--- KlayGE/Core/Src/Render/DeferredRenderingLayer.cpp
+++ KlayGE/Core/Src/Render/DeferredRenderingLayer.cpp
@@ -1,32 +1,32 @@
 /**
-* @file DeferredRenderingLayer.cpp
-* @author Minmin Gong
-*
-* @section DESCRIPTION
-*
-* This source file is part of KlayGE
-* For the latest info, see http ://www.klayge.org
-*
-* @section LICENSE
-*
-* This program is free software; you can redistribute it and / or modify
-* it under the terms of the GNU General Public License as published
-* by the Free Software Foundation; either version 2 of the License, or
-*(at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-*but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 - 1307 USA
-*
-* You may alternatively use this source under the terms of
-* the KlayGE Proprietary License(KPL).You can obtained such a license
-* from http ://www.klayge.org/licensing/.
-*/
+ * @file DeferredRenderingLayer.cpp
+ * @author Minmin Gong
+ *
+ * @section DESCRIPTION
+ *
+ * This source file is part of KlayGE
+ * For the latest info, see http://www.klayge.org
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * You may alternatively use this source under the terms of
+ * the KlayGE Proprietary License (KPL). You can obtained such a license
+ * from http://www.klayge.org/licensing/.
+ */
 
 #include <KlayGE/KlayGE.hpp>
 #include <KFL/Util.hpp>
@@ -1035,6 +1035,7 @@ namespace KlayGE
 				min_max_depth_fmt = depth_fmt;
 			}
 
+			pvp.g_buffer_min_max_depth_texs.clear();
 			if (cs_tbdr_)
 			{
 				uint32_t w = std::max(1U, (width + TILE_SIZE - 1) / TILE_SIZE);
@@ -2716,7 +2717,10 @@ namespace KlayGE
 			else 
 			{
 				pp_chain->OutputPin(0, filtered_sm_2d_texs_[sm_light_indices_[org_no].first]);
-				sm_tex_->CopyToTexture(*unfiltered_sm_2d_texs_[sm_light_indices_[org_no].first]);
+				if (has_sss_objs_ && translucency_enabled_)
+				{
+					sm_tex_->CopyToTexture(*unfiltered_sm_2d_texs_[sm_light_indices_[org_no].first]);
+				}
 			}
 		}
 
