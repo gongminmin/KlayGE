4f7d3cb08c8559aae78f494ce1653468cc78130a
Minmin Gong <gongminmin@msn.com>
2015-12-14 22:25:16 -0800

    KlayGE: Tools: Collect more information in HWCollect. (ticket #313)

--- KlayGE/Tools/src/HWCollect/HWCollect.cpp
+++ KlayGE/Tools/src/HWCollect/HWCollect.cpp
@@ -29,37 +29,42 @@
  */
 
 #include <KlayGE/KlayGE.hpp>
-#include <KlayGE/HWDetect.hpp>
-
+#define INITGUID
+#include <KFL/Util.hpp>
 #include <KFL/CpuInfo.hpp>
+#include <KlayGE/HWDetect.hpp>
 
 #if defined KLAYGE_PLATFORM_WINDOWS
 	#include <windows.h>
+	#include <dxgi1_2.h>
 #endif
 
+#include <string>
+#include <sstream>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 using namespace KlayGE;
 
-void DetectOSInfo()
+void DetectOSInfo(std::ostream& os)
 {
 #if defined KLAYGE_PLATFORM_WINDOWS
 	SYSTEM_INFO si;
 
 #if defined KLAYGE_PLATFORM_WINDOWS_DESKTOP
-	typedef NTSTATUS(WINAPI *RtlGetVersionFunc)(OSVERSIONINFOEXW* pVersionInformation);
+	typedef NTSTATUS (WINAPI *RtlGetVersionFunc)(OSVERSIONINFOEXW* pVersionInformation);
 	HMODULE ntdll = GetModuleHandleW(L"ntdll.dll");
-	RtlGetVersionFunc RtlGetVersion = reinterpret_cast<RtlGetVersionFunc>(GetProcAddress(ntdll, "RtlGetVersion"));
+	RtlGetVersionFunc RtlGetVersion = reinterpret_cast<RtlGetVersionFunc>(::GetProcAddress(ntdll, "RtlGetVersion"));
 	OSVERSIONINFOEXW os_ver_info;
 	os_ver_info.dwOSVersionInfoSize = sizeof(os_ver_info);
 	RtlGetVersion(&os_ver_info);
 
-	GetSystemInfo(&si);
+	::GetSystemInfo(&si);
 
-	int server_r2 = GetSystemMetrics(SM_SERVERR2);
+	int server_r2 = ::GetSystemMetrics(SM_SERVERR2);
 
-	cout << "Windows ";
+	os << "Windows ";
 
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724833%28v=vs.85%29.aspx
 	switch (os_ver_info.dwMajorVersion)
@@ -68,34 +73,34 @@ void DetectOSInfo()
 		switch (os_ver_info.dwMinorVersion)
 		{
 		case 0:
-			cout << "2000";
+			os << "2000";
 			break;
 
 		case 1:
-			cout << "XP";
+			os << "XP";
 			break;
 
 		case 2:
 			if ((VER_NT_WORKSTATION == os_ver_info.wProductType) && (PROCESSOR_ARCHITECTURE_AMD64 == si.wProcessorArchitecture))
 			{
-				cout << "XP Professional x64 Edition";
+				os << "XP Professional x64 Edition";
 			}
 			else if (os_ver_info.wSuiteMask & VER_SUITE_WH_SERVER)
 			{
-				cout << "Home Server";
+				os << "Home Server";
 			}
 			else if (0 == server_r2)
 			{
-				cout << "Server 2003";
+				os << "Server 2003";
 			}
 			else
 			{
-				cout << "Server 2003 R2";
+				os << "Server 2003 R2";
 			}
 			break;
 
 		default:
-			cout << "(unknown edition)";
+			os << "(unknown edition)";
 			break;
 		}
 		break;
@@ -106,49 +111,49 @@ void DetectOSInfo()
 		case 0:
 			if (VER_NT_WORKSTATION == os_ver_info.wProductType)
 			{
-				cout << "Vista";
+				os << "Vista";
 			}
 			else
 			{
-				cout << "Server 2008";
+				os << "Server 2008";
 			}
 			break;
 
 		case 1:
 			if (VER_NT_WORKSTATION == os_ver_info.wProductType)
 			{
-				cout << "7";
+				os << "7";
 			}
 			else
 			{
-				cout << "Server 2008 R2";
+				os << "Server 2008 R2";
 			}
 			break;
 
 		case 2:
 			if (VER_NT_WORKSTATION == os_ver_info.wProductType)
 			{
-				cout << "8";
+				os << "8";
 			}
 			else
 			{
-				cout << "Server 2012";
+				os << "Server 2012";
 			}
 			break;
 
 		case 3:
 			if (VER_NT_WORKSTATION == os_ver_info.wProductType)
 			{
-				cout << "8.1";
+				os << "8.1";
 			}
 			else
 			{
-				cout << "Server 2012 R2";
+				os << "Server 2012 R2";
 			}
 			break;
 
 		default:
-			cout << "(unknown edition)";
+			os << "(unknown edition)";
 			break;
 		}
 		break;
@@ -159,145 +164,243 @@ void DetectOSInfo()
 		case 0:
 			if (VER_NT_WORKSTATION == os_ver_info.wProductType)
 			{
-				cout << "10";
+				os << "10";
 			}
 			else
 			{
-				cout << "Server 2016";
+				os << "Server 2016";
 			}
 			break;
 
 		default:
-			cout << "(unknown edition)";
+			os << "(unknown edition)";
 			break;
 		}
 		break;
 
 	default:
-		cout << "Unknown";
+		os << "Unknown";
 		break;
 	}
 
-	cout << ' ' << os_ver_info.dwMajorVersion << '.' << os_ver_info.dwMinorVersion
+	os << ' ' << os_ver_info.dwMajorVersion << '.' << os_ver_info.dwMinorVersion
 		<< " (" << os_ver_info.dwBuildNumber << ')';
 	if ((os_ver_info.wServicePackMajor != 0) || (os_ver_info.wServicePackMinor != 0))
 	{
-		cout << "SP " << os_ver_info.wServicePackMajor << '.' << os_ver_info.wServicePackMinor;
+		os << "SP " << os_ver_info.wServicePackMajor << '.' << os_ver_info.wServicePackMinor;
 	}
 #else
 #if defined KLAYGE_PLATFORM_WINDOWS_UWP
-	cout << "Windows UWP";
+	os << "Windows UWP";
 #elif defined KLAYGE_PLATFORM_WINDOWS_STORE
-	cout << "Windows Store";
+	os << "Windows Store";
 #elif defined KLAYGE_PLATFORM_WINDOWS_PHONE
-	cout << "Windows Phone";
+	os << "Windows Phone";
 #endif
 
 	::GetNativeSystemInfo(&si);
 #endif
 
-	cout << ' ';
+	os << ' ';
 	switch (si.wProcessorArchitecture)
 	{
 	case PROCESSOR_ARCHITECTURE_INTEL:
-		cout << "x86";
+		os << "x86";
 		break;
 	case PROCESSOR_ARCHITECTURE_ARM:
-		cout << "arm";
+		os << "arm";
 		break;
 	case PROCESSOR_ARCHITECTURE_IA64:
-		cout << "ia64";
+		os << "ia64";
 		break;
 	case PROCESSOR_ARCHITECTURE_AMD64:
-		cout << "x64";
+		os << "x64";
 		break;
 	case PROCESSOR_ARCHITECTURE_ARM64:
-		cout << "arm64";
+		os << "arm64";
 		break;
 
 	default:
-		cout << "Unknown architecture";
+		os << "Unknown architecture";
 		break;
 	}
-
-	cout << endl;
 #elif defined KLAYGE_PLATFORM_DARWIN
-	cout << "Darwin" << endl;
+	os << "Darwin";
 #elif defined KLAYGE_PLATFORM_LINUX
-	cout << "Linux" << endl;
+	os << "Linux";
 #elif defined KLAYGE_PLATFORM_ANDROID
-	cout << "Android" << endl;
+	os << "Android";
 #elif defined KLAYGE_PLATFORM_IOS
-	cout << "iOS" << endl;
+	os << "iOS";
 #endif
+
+	os << endl;
 }
 
-void DetectCpuInfo()
+void DetectCpuInfo(std::ostream& os)
 {
 	CPUInfo cpu_info;
 	if ("GenuineIntel" == cpu_info.CPUString())
 	{
-		cout << "Intel CPU";
+		os << "Intel CPU";
 	}
 	else if ("AuthenticAMD" == cpu_info.CPUString())
 	{
-		cout << "AMD CPU";
+		os << "AMD CPU";
 	}
 	else
 	{
-		cout << "Unknown CPU";
+		os << "Unknown CPU";
 	}
-	cout << endl;
-	cout << "Brand: " << cpu_info.CPUBrandString() << endl;
-	cout << "Cores: " << cpu_info.NumCores() << endl;
-	cout << "Threads: " << cpu_info.NumHWThreads() << endl;
+	os << endl;
+	os << "Brand: " << cpu_info.CPUBrandString() << endl;
+	os << "Cores: " << cpu_info.NumCores() << endl;
+	os << "Threads: " << cpu_info.NumHWThreads() << endl;
 }
 
-void DetectMainboardInfo()
+void DetectGpuInfo(std::ostream& os)
+{
+#if defined KLAYGE_PLATFORM_WINDOWS
+	typedef HRESULT (WINAPI *CreateDXGIFactory1Func)(REFIID riid, void** ppFactory);
+
+#if defined KLAYGE_PLATFORM_WINDOWS_DESKTOP
+	HMODULE dxgi = ::LoadLibraryEx(L"dxgi.dll", nullptr, 0);
+	if (!dxgi)
+	{
+		os << "Unknown GPU";
+		return;
+	}
+	CreateDXGIFactory1Func DynamicCreateDXGIFactory1 = (CreateDXGIFactory1Func)::GetProcAddress(dxgi, "CreateDXGIFactory1");
+	if (!DynamicCreateDXGIFactory1)
+	{
+		os << "Unknown GPU";
+		return;
+	}
+#else
+	CreateDXGIFactory1Func DynamicCreateDXGIFactory1 = CreateDXGIFactory1;
+#endif
+
+	IDXGIFactory1* factory;
+	if (SUCCEEDED((*DynamicCreateDXGIFactory1)(IID_IDXGIFactory1, reinterpret_cast<void**>(&factory))))
+	{
+		UINT adapter_no = 0;
+		IDXGIAdapter1* adapter = nullptr;
+		while (factory->EnumAdapters1(adapter_no, &adapter) != DXGI_ERROR_NOT_FOUND)
+		{
+			if (adapter != nullptr)
+			{
+				DXGI_ADAPTER_DESC1 adapter_desc;
+				adapter->GetDesc1(&adapter_desc);
+
+				IDXGIAdapter2* adapter2;
+				adapter->QueryInterface(IID_IDXGIAdapter2, reinterpret_cast<void**>(&adapter2));
+				if (adapter2 != nullptr)
+				{
+					DXGI_ADAPTER_DESC2 desc2;
+					adapter2->GetDesc2(&desc2);
+					memcpy(adapter_desc.Description, desc2.Description, sizeof(desc2.Description));
+					adapter_desc.VendorId = desc2.VendorId;
+					adapter_desc.DeviceId = desc2.DeviceId;
+					adapter_desc.SubSysId = desc2.SubSysId;
+					adapter_desc.Revision = desc2.Revision;
+					adapter_desc.DedicatedVideoMemory = desc2.DedicatedVideoMemory;
+					adapter_desc.DedicatedSystemMemory = desc2.DedicatedSystemMemory;
+					adapter_desc.SharedSystemMemory = desc2.SharedSystemMemory;
+					adapter_desc.AdapterLuid = desc2.AdapterLuid;
+					adapter_desc.Flags = desc2.Flags;
+					adapter2->Release();
+				}
+
+				adapter->Release();
+
+				if (adapter_desc.Flags != DXGI_ADAPTER_FLAG_SOFTWARE)
+				{
+					os << "Adapter " << adapter_no << endl << endl;
+					std::string description;
+					Convert(description, adapter_desc.Description);
+					os << "Brand: " << description << endl;
+					os << "Vendor ID: " << std::hex << std::uppercase << adapter_desc.VendorId << endl;
+					os << "Device ID: " << std::hex << std::uppercase << adapter_desc.DeviceId << endl;
+					os << "Revision: " << std::hex << std::uppercase << adapter_desc.Revision << endl;
+					os << "Dedicated video memory: " << std::dec << adapter_desc.DedicatedVideoMemory / 1024 / 1024 << " MB" << endl;
+					os << "Dedicated system memory: " << std::dec << adapter_desc.DedicatedSystemMemory / 1024 / 1024 << " MB" << endl;
+					os << "Shared system memory: " << std::dec << adapter_desc.SharedSystemMemory / 1024 / 1024 << " MB" << endl;
+				}
+			}
+
+			++ adapter_no;
+		}
+	}
+
+#if defined KLAYGE_PLATFORM_WINDOWS_DESKTOP
+	::FreeLibrary(dxgi);
+#endif
+#endif
+}
+
+void DetectMainboardInfo(std::ostream& os)
 {
 	if (SMBios::Instance().Available())
 	{
-		cout << "SMBios Ver: " << static_cast<int>(SMBios::Instance().MajorVersion())
+		os << "SMBios Ver: " << static_cast<int>(SMBios::Instance().MajorVersion())
 			<< '.' << static_cast<int>(SMBios::Instance().MinorVersion()) << endl;
-		cout << endl;
+		os << endl;
 
 		Mainboard mainboard;
-		cout << "Mainboard: " << mainboard.Manufacturer() << ' ' << mainboard.Product() << endl;
-		cout << "Bios: " << mainboard.BiosVendor() << ' ' << mainboard.BiosVersion() << endl;
-		cout << "Release Date: " << mainboard.BiosReleaseDate() << endl;
-		cout << endl;
+		os << "Mainboard: " << mainboard.Manufacturer() << ' ' << mainboard.Product() << endl;
+		os << "Bios: " << mainboard.BiosVendor() << ' ' << mainboard.BiosVersion() << endl;
+		os << "Release Date: " << mainboard.BiosReleaseDate() << endl;
+		os << endl;
 
 		MemoryBank mem;
 		for (size_t i = 0; i < mem.SlotCount(); ++i)
 		{
 			if (mem[i].size != 0)
 			{
-				cout << "Memory slot " << i << ": ";
-				cout << mem[i].manufacturer << ' ' << mem[i].size << " MB" << endl;
-				cout << "Part #: " << mem[i].part_number << endl;
-				cout << "Serial #: " << mem[i].serial_num << endl;
-				cout << endl;
+				os << "Memory slot " << i << ": ";
+				os << mem[i].manufacturer << ' ' << mem[i].size << " MB" << endl;
+				os << "Part #: " << mem[i].part_number << endl;
+				os << "Serial #: " << mem[i].serial_num << endl;
+				os << endl;
 			}
 		}
 	}
 	else
 	{
-		cout << "Could not read from SMBios." << endl;
+		os << "Could not read from SMBios." << endl;
 	}
 }
 
 int main()
 {
-	cout << "=== OS information ===" << endl;
-	DetectOSInfo();
-	cout << endl;
+	std::stringstream ss;
 
-	cout << "=== CPU information ===" << endl;
-	DetectCpuInfo();
-	cout << endl;
+	ss << "=== OS information ===" << endl;
+	DetectOSInfo(ss);
+	ss << endl;
 
-	cout << "=== Mainboard information ===" << endl;
-	DetectMainboardInfo();
+	ss << "=== CPU information ===" << endl;
+	DetectCpuInfo(ss);
+	ss << endl;
+
+	ss << "=== CPU information ===" << endl;
+	DetectGpuInfo(ss);
+	ss << endl;
+
+	ss << "=== Mainboard information ===" << endl;
+	DetectMainboardInfo(ss);
+	ss << endl;
+
+	cout << ss.str();
+
+	cout << "Do you like to save this informatin to a file? (y/N) ";
+	int ch = cin.get();
+	if (('y' == ch) || ('Y' == ch))
+	{
+		ofstream ofs("HWCollect.txt");
+		ofs << ss.str();
+	}
+
 	cout << endl;
 
 	return 0;
