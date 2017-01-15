10391f71484a7819f2b423517a6255f72b1631c0
Minmin Gong <gongminmin@msn.com>
2017-01-14 23:11:17 -0800

    Build: Remove Windows phone configuration from cfg_build_default.

--- cfg_build_default.py
+++ cfg_build_default.py
@@ -5,38 +5,41 @@
 # Project type.
 #   On Windows desktop, could be "vs2017", "vs2015", "make", "auto".
 #   On Windows store, could be "vs2017", "vs2015", "auto".
-#   On Windows phone, could be "vs2017", "vs2015", "auto".
 #   On Android, could be "make", "auto".
 #   On Linux, could be "make", "auto".
-#   On MacOSX, could be "xcode", "auto".
+#   On macOS, could be "xcode", "auto".
 #   On iOS, could be "xcode", "auto".
 project			= "auto"
 
 # Compiler name.
 #   On Windows desktop, could be "vc141", "vc140", "mingw", "auto".
 #   On Windows store, could be "vc141", "vc140", "auto".
-#   On Windows phone, could be "vc141", "vc140", "auto".
 #   On Android, could be "clang", "auto".
 #   On Linux, could be "gcc", "auto".
-#   On MacOSX, could be "clang", "auto".
+#   On macOS, could be "clang", "auto".
 #   On iOS, could be "clang", "auto".
 compiler		= "auto"
 
 # Target CPU architecture.
 #   On Windows desktop, could be "x86", "x64".
 #   On Windows store, could be "arm", "x86", "x64".
-#   On Windows phone, could be "arm", "x86".
 #   On Android, cound be "armeabi-v7a", "arm64-v8a", "x86", "x86_64".
 #   On Linux, could be "x86", "x64".
-#   On MacOSX, could be "x64".
+#   On macOS, could be "x64".
 #   On iOS, could be "arm", "x86".
 arch			= ("x64", )
 
 # Configuration. Could be "Debug", "Release", "MinSizeRel", "RelWithDebInfo".
 config			= ("Debug", "RelWithDebInfo")
 
-# Target platform for cross compiling. Could be "android" plus version number, "win_store", "win_phone" plus version number, "ios", or "auto".
+# Target platform for cross compiling.
+#   On Windows desktop, could be "auto".
+#   On Windows store, could be "win_store 10.0".
+#   On Android, cound be "android 5.1", "android 6.0", "android 7.0", "android 7.1".
+#   On Linux, could be "auto".
+#   On macOS, could be "auto".
+#   On iOS, could be "ios".
 target			= "auto"
 
-# A name for offline FXML compiling. Could be one of "d3d_11_0", "d3d_10_1", "d3d_10_0", "d3d_9_3", "d3d_9_1", "gles_2_0", "gles_3_0", "gles_3_1", "gles_3_2", or "auto".
+# A name for offline FXML compiling. Could be one of "d3d_11_0", "d3d_10_1", "d3d_10_0", "d3d_9_3", "gles_2_0", "gles_3_0", "gles_3_1", "gles_3_2", or "auto".
 shader_platform_name	= "auto"
