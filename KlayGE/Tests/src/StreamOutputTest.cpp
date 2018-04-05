728ca7d7de9cd5558c40cab200091a7fb4295bf9
Minmin Gong <gongminmin@msn.com>
2018-04-04 23:11:02 -0700

    KlayGE: Test: Add DrawIndirect test.

--- KlayGE/Tests/src/StreamOutputTest.cpp
+++ KlayGE/Tests/src/StreamOutputTest.cpp
@@ -63,6 +63,8 @@ public:
 	{
 		auto const & caps = Context::Instance().RenderFactoryInstance().RenderEngineInstance().DeviceCaps();
 		stream_output_support_ = caps.stream_output_support;
+		draw_indirect_support_ = caps.draw_indirect_support;
+		uavs_at_every_stage_support_ = caps.uavs_at_every_stage_support;
 		gs_support_ = caps.gs_support;
 	}
 
@@ -180,6 +182,57 @@ public:
 		}
 	}
 
+	void TestDrawIndirectCopyBuffer(float tolerance)
+	{
+		if (!stream_output_support_ || !gs_support_ || !draw_indirect_support_ || !uavs_at_every_stage_support_)
+		{
+			return;
+		}
+
+		std::ranlux24_base gen;
+		std::uniform_int_distribution<> dis(-100, 100);
+
+		uint32_t const num_vertices = 1024;
+
+		auto& rf = Context::Instance().RenderFactoryInstance();
+		auto vb_in = rf.MakeVertexBuffer(BU_Dynamic, EAH_GPU_Read | EAH_CPU_Write, num_vertices * sizeof(float4), nullptr);
+		auto vb_sanity = rf.MakeVertexBuffer(BU_Dynamic, EAH_CPU_Read | EAH_CPU_Write, num_vertices * sizeof(float4), nullptr);
+
+		for (uint32_t i = 0; i < 10; ++ i)
+		{
+			uint32_t sanity_size = 0;
+			{
+				GraphicsBuffer::Mapper input_mapper(*vb_in, BA_Write_Only);
+				float4* input_data = input_mapper.Pointer<float4>();
+				GraphicsBuffer::Mapper sanity_mapper(*vb_sanity, BA_Write_Only);
+				float4* sanity_data = sanity_mapper.Pointer<float4>();
+				for (uint32_t j = 0; j < num_vertices; ++ j)
+				{
+					float const x = static_cast<float>(dis(gen));
+					float const y = static_cast<float>(dis(gen));
+					float const z = static_cast<float>(dis(gen));
+					float const w = static_cast<float>(dis(gen));
+
+					input_data[j] = float4(x, y, z, w);
+
+					if (w > 0)
+					{
+						sanity_data[sanity_size] = input_data[j];
+						++ sanity_size;
+					}
+				}
+			}
+
+			uint64_t output_primitives;
+			auto vb_out = DrawIndirectCopyBuffer(vb_in, output_primitives);
+
+			EXPECT_TRUE(output_primitives == sanity_size);
+			EXPECT_TRUE(CompareBuffer(*vb_sanity, 0,
+				*vb_out, 0,
+				sanity_size * 4, tolerance));
+		}
+	}
+
 private:
 	GraphicsBufferPtr CopyBuffer(GraphicsBufferPtr const & vb_in)
 	{
@@ -259,8 +312,66 @@ private:
 		return vb_out;
 	}
 
+	GraphicsBufferPtr DrawIndirectCopyBuffer(GraphicsBufferPtr const & vb_in, uint64_t& output_primitives)
+	{
+		auto& rf = Context::Instance().RenderFactoryInstance();
+
+		auto effect = SyncLoadRenderEffect("StreamOutput/StreamOutputTest.fxml");
+		auto tech = effect->TechniqueByName("ConditionalCopyBufferRw");
+
+		uint32_t indirect_args[] = { 0, 1, 0, 0 };
+		auto vb_num = rf.MakeVertexBuffer(BU_Dynamic, EAH_GPU_Write | EAH_DrawIndirectArgs | EAH_GPU_Unordered | EAH_Raw,
+			sizeof(indirect_args), indirect_args, EF_R32UI);
+
+		auto fb = rf.MakeFrameBuffer();
+		auto vb_num_uav = rf.MakeGraphicsBufferUnorderedAccessView(*vb_num, EF_R32UI);
+		fb->AttachUAV(0, vb_num_uav);
+
+		*(effect->ParameterByName("rw_output_primitives_buff")) = vb_num;
+
+		auto rl_in = rf.MakeRenderLayout();
+		rl_in->TopologyType(RenderLayout::TT_PointList);
+		rl_in->BindVertexStream(vb_in, VertexElement(VEU_Position, 0, EF_ABGR32F));
+
+		auto rl_intermediate = rf.MakeRenderLayout();
+		rl_intermediate->TopologyType(RenderLayout::TT_PointList);
+		auto vb_intermediate = rf.MakeVertexBuffer(BU_Dynamic, EAH_GPU_Write, vb_in->Size(), nullptr);
+		rl_intermediate->BindVertexStream(vb_intermediate, VertexElement(VEU_Position, 0, EF_ABGR32F));
+
+		auto& re = rf.RenderEngineInstance();
+		re.BindSOBuffers(rl_intermediate);
+		re.BindFrameBuffer(fb);
+		re.Render(*effect, *tech, *rl_in);
+		re.BindFrameBuffer(FrameBufferPtr());
+		re.BindSOBuffers(RenderLayoutPtr());
+
+		tech = effect->TechniqueByName("CopyBuffer");
+
+		auto rl_out = rf.MakeRenderLayout();
+		rl_out->TopologyType(RenderLayout::TT_PointList);
+		auto vb_out = rf.MakeVertexBuffer(BU_Dynamic, EAH_GPU_Write, vb_in->Size(), nullptr);
+		rl_out->BindVertexStream(vb_out, VertexElement(VEU_Position, 0, EF_ABGR32F));
+
+		rl_intermediate->BindIndirectArgs(vb_num);
+		rl_intermediate->IndirectArgsOffset(0);
+		re.BindSOBuffers(rl_out);
+		re.Render(*effect, *tech, *rl_intermediate);
+		re.BindSOBuffers(RenderLayoutPtr());
+
+		auto vb_num_cpu = rf.MakeVertexBuffer(BU_Static, EAH_CPU_Read, vb_num->Size(), nullptr);
+		vb_num->CopyToBuffer(*vb_num_cpu);
+		{
+			GraphicsBuffer::Mapper mapper(*vb_num_cpu, BA_Read_Only);
+			output_primitives = *mapper.Pointer<uint32_t>();
+		}
+
+		return vb_out;
+	}
+
 private:
 	bool stream_output_support_;
+	bool draw_indirect_support_;
+	bool uavs_at_every_stage_support_;
 	bool gs_support_;
 };
 
@@ -277,4 +388,9 @@ TEST_F(StreamOutputTest, VertexIDToBuffer)
 TEST_F(StreamOutputTest, ConditionalCopyBuffer)
 {
 	TestConditionalCopyBuffer(1.0f / 255);
+}
+
+TEST_F(StreamOutputTest, DrawIndirectCopyBuffer)
+{
+	TestDrawIndirectCopyBuffer(1.0f / 255);
 }
