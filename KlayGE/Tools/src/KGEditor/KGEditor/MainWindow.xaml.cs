d34606e6479b32a06fd30304bc2f5cd367673e92
Minmin Gong <gongminmin@msn.com>
2019-08-03 21:33:29 -0700

    KlayGE: Tools: Remove unnecessary usings in C# code

--- KlayGE/Tools/src/KGEditor/KGEditor/MainWindow.xaml.cs
+++ KlayGE/Tools/src/KGEditor/KGEditor/MainWindow.xaml.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using KlayGE;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Windows.Controls.Ribbon;
 using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
-using KlayGE;
 
 namespace KGEditor
 {
