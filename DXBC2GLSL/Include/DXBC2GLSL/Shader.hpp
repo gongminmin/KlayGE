gongminmin/KlayGE

commit aaff35d9ca76f8e842fb057464a46e89ab338dc3

KlayGE: Rendering: Non-template implemented particle system. (ticket #160)

KlayGE/Core/Src/UI/UIPolylineEditBox.cpp
@@ -110,8 +110,8 @@ namespace KlayGE
 
 	int UIPolylineEditBox::AddCtrlPoint(float pos, float value)
 	{
-		pos = std::max(std::min(pos, 1.0f), 0.0f);
-		value = std::max(std::min(value, 1.0f), 0.0f);
+		pos = MathLib::clamp(pos, 0.0f, 1.0f);
+		value = MathLib::clamp(pos, 0.0f, 1.0f);
 
 		int index;
 		if (ctrl_points_.size() >= 1)
@@ -139,7 +139,7 @@ namespace KlayGE
 
 	int UIPolylineEditBox::AddCtrlPoint(float pos)
 	{
-		pos = std::max(std::min(pos, 1.0f), 0.0f);
+		pos = MathLib::clamp(pos, 0.0f, 1.0f);
 		float value = this->GetValue(pos);
 
 		return this->AddCtrlPoint(pos, value);
@@ -188,7 +188,7 @@ namespace KlayGE
 	float2 UIPolylineEditBox::PtFromCoord(int x, int y) const
 	{
 		return float2(static_cast<float>(x - bounding_box_.left()) / bounding_box_.Width(),
-			static_cast<float>(y - bounding_box_.top()) / bounding_box_.Height());
+			static_cast<float>(bounding_box_.bottom() - 1 - y) / bounding_box_.Height());
 	}
 
 	float UIPolylineEditBox::GetValue(float pos) const
@@ -320,14 +320,14 @@ namespace KlayGE
 						if (0 == this->ActivePoint())
 						{
 							p.x() = 0;
-							p.y() = std::min(std::max(p.y(), 0.0f), 1.0f);
+							p.y() = MathLib::clamp(p.y(), 0.0f, 1.0f);
 						}
 						else
 						{
 							if (static_cast<int>(this->NumCtrlPoints() - 1) == this->ActivePoint())
 							{
 								p.x() = 1;
-								p.y() = std::min(std::max(p.y(), 0.0f), 1.0f);
+								p.y() = MathLib::clamp(p.y(), 0.0f, 1.0f);
 							}
 						}
 						if (this->ActivePoint() < static_cast<int>(this->NumCtrlPoints() - 1))
@@ -382,7 +382,8 @@ namespace KlayGE
 				{
 					offset = -1;
 				}
-				dlg->DrawRect(Rect_T<int32_t>(x + offset, bounding_box_.top(), x + 1, bounding_box_.bottom()), 0, elements_[COORDLINE_INDEX]->TextureColor().Current);
+				dlg->DrawRect(Rect_T<int32_t>(x + offset, bounding_box_.top(), x + 1, bounding_box_.bottom()),
+					0, elements_[COORDLINE_INDEX]->TextureColor().Current);
 			}
 			for (size_t i = 0; i < 11; ++ i)
 			{
@@ -392,7 +393,8 @@ namespace KlayGE
 				{
 					offset = -1;
 				}
-				dlg->DrawRect(Rect_T<int32_t>(bounding_box_.left(), y + offset, bounding_box_.right(), y + 1), 0, elements_[COORDLINE_INDEX]->TextureColor().Current);
+				dlg->DrawRect(Rect_T<int32_t>(bounding_box_.left(), y + offset, bounding_box_.right(), y + 1),
+					0, elements_[COORDLINE_INDEX]->TextureColor().Current);
 			}
 
 			{
@@ -400,15 +402,15 @@ namespace KlayGE
 				for (size_t i = 0; i < ctrl_points_.size() - 1; ++ i)
 				{
 					float2 dir = ctrl_points_[i + 1] - ctrl_points_[i + 0];
-					dir = MathLib::normalize(float2(dir.y(), -dir.x())) / 2.0f;
-					float x0 = bounding_box_.left() + ctrl_points_[i + 1].x() * bounding_box_.Width() - dir.x();
-					float x1 = bounding_box_.left() + ctrl_points_[i + 0].x() * bounding_box_.Width() - dir.x();
-					float x2 = bounding_box_.left() + ctrl_points_[i + 0].x() * bounding_box_.Width() + dir.x();
-					float x3 = bounding_box_.left() + ctrl_points_[i + 1].x() * bounding_box_.Width() + dir.x();
-					float y0 = bounding_box_.top() + ctrl_points_[i + 1].y() * bounding_box_.Height() - dir.y();
-					float y1 = bounding_box_.top() + ctrl_points_[i + 0].y() * bounding_box_.Height() - dir.y();
-					float y2 = bounding_box_.top() + ctrl_points_[i + 0].y() * bounding_box_.Height() + dir.y();
-					float y3 = bounding_box_.top() + ctrl_points_[i + 1].y() * bounding_box_.Height() + dir.y();
+					dir = MathLib::normalize(float2(dir.y(), dir.x())) / 2.0f;
+					float x0 = bounding_box_.left() + ctrl_points_[i + 1].x() * bounding_box_.Width() + dir.x();
+					float x1 = bounding_box_.left() + ctrl_points_[i + 0].x() * bounding_box_.Width() + dir.x();
+					float x2 = bounding_box_.left() + ctrl_points_[i + 0].x() * bounding_box_.Width() - dir.x();
+					float x3 = bounding_box_.left() + ctrl_points_[i + 1].x() * bounding_box_.Width() - dir.x();
+					float y0 = bounding_box_.bottom() - 1 - ctrl_points_[i + 1].y() * bounding_box_.Height() + dir.y();
+					float y1 = bounding_box_.bottom() - 1 - ctrl_points_[i + 0].y() * bounding_box_.Height() + dir.y();
+					float y2 = bounding_box_.bottom() - 1 - ctrl_points_[i + 0].y() * bounding_box_.Height() - dir.y();
+					float y3 = bounding_box_.bottom() - 1 - ctrl_points_[i + 1].y() * bounding_box_.Height() - dir.y();
 					UIManager::VertexFormat vd[] = 
 					{
 						UIManager::VertexFormat(float3(x0, y0, 0), clr, float2(0, 0)),
@@ -429,8 +431,8 @@ namespace KlayGE
 
 					int32_t x0 = bounding_box_.left() + static_cast<int32_t>(ctrl_points_[i].x() * bounding_box_.Width()) - offset;
 					int32_t x1 = bounding_box_.left() + static_cast<int32_t>(ctrl_points_[i].x() * bounding_box_.Width()) + offset;
-					int32_t y0 = bounding_box_.top() + static_cast<int32_t>(ctrl_points_[i].y() * bounding_box_.Height()) - offset;
-					int32_t y1 = bounding_box_.top() + static_cast<int32_t>(ctrl_points_[i].y() * bounding_box_.Height()) + offset;
+					int32_t y0 = bounding_box_.bottom() - 1 - static_cast<int32_t>(ctrl_points_[i].y() * bounding_box_.Height()) - offset;
+					int32_t y1 = bounding_box_.bottom() - 1 - static_cast<int32_t>(ctrl_points_[i].y() * bounding_box_.Height()) + offset;
 					dlg->DrawRect(Rect_T<int32_t>(x0, y0, x1, y1), 0, (i == this->ActivePoint()) ? clr_active : clr_normal);
 				}
 			}

KlayGE/Core/Include/KlayGE/ParticleSystem.hpp
@@ -1,14 +1,32 @@
-// ParticleSystem.hpp
-// KlayGE 粒子系统类 头文件
-// Ver 3.4.0
-// 版权所有(C) 龚敏敏, 2006
-// Homepage: http://www.klayge.org
-//
-// 3.4.0
-// 初次建立 (2006.7.12)
-//
-// 修改记录
-//////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file ParticleSystem.hpp
+ * @author Minmin Gong
+ *
+ * @section DESCRIPTION
+ *
+ * This source file is part of KlayGE
+ * For the latest info, see http://www.klayge.org
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * You may alternatively use this source under the terms of
+ * the KlayGE Proprietary License (KPL). You can obtained such a license
+ * from http://www.klayge.org/licensing/.
+ */
 
 #ifndef _PARTICLESYSTEM_HPP
 #define _PARTICLESYSTEM_HPP
@@ -22,96 +40,42 @@
 
 namespace KlayGE
 {
-	template <typename ParticleType>
-	class ParticleSystem
+	struct Particle
+	{
+		float3 pos;
+		float3 vel;
+		float life;
+		float spin;
+		float birth_time;
+		float size;
+		Color color;
+	};
+
+	class KLAYGE_CORE_API ParticleSystem
 	{
 	public:
 		ParticleSystem(uint32_t max_num_particles,
-						function<void(ParticleType& par, float4x4 const & mat)> const & gen_func,
-						function<void(ParticleType& par, float elapse_time)> const & update_func)
-			: gen_func_(gen_func), update_func_(update_func),
-				particles_(max_num_particles),
-				inv_emit_freq_(0), accumulate_time_(0),
-				model_mat_(float4x4::Identity())
-		{
-		}
+			function<void(Particle& par, float4x4 const & mat)> const & emitter_func,
+			function<void(Particle& par, float elapse_time)> const & update_func);
 
-		void ModelMatrix(float4x4 const & model)
-		{
-			model_mat_ = model;
-		}
+		void ModelMatrix(float4x4 const & model);
+		float4x4 const & ModelMatrix() const;
 
-		float4x4 const & ModelMatrix() const
-		{
-			return model_mat_;
-		}
+		void Frequency(float freq);
+		float Frequency() const;
 
-		void Frequency(float freq)
-		{
-			inv_emit_freq_ = 1.0f / freq;
+		void Update(float app_time, float elapsed_time);
 
-			float time = 0;
-			typedef typename KLAYGE_DECLTYPE(particles_) ParticlesType;
-			KLAYGE_FOREACH(typename ParticlesType::reference particle, particles_)
-			{
-				particle.life = -1;
-				particle.birth_time = time;
-				time += inv_emit_freq_;
-			}
-		}
+		uint32_t NumParticles() const;
 
-		float Frequency() const
-		{
-			return 1.0f / inv_emit_freq_;
-		}
-
-		void Update(float /*app_time*/, float elapsed_time)
-		{
-			accumulate_time_ += elapsed_time;
-			if (accumulate_time_ >= particles_.size() * inv_emit_freq_)
-			{
-				accumulate_time_ = 0;
-			}
-
-			typedef typename KLAYGE_DECLTYPE(particles_) ParticlesType;
-			KLAYGE_FOREACH(typename ParticlesType::reference particle, particles_)
-			{
-				if (particle.life > 0)
-				{
-					update_func_(particle, elapsed_time);
-				}
-				else
-				{
-					float const t = accumulate_time_ - particle.birth_time;
-					if ((t >= 0) && (t < elapsed_time))
-					{
-						gen_func_(particle, model_mat_);
-					}
-				}
-			}
-		}
-
-		uint32_t NumParticles() const
-		{
-			return static_cast<uint32_t>(particles_.size());
-		}
-
-		ParticleType const & GetParticle(uint32_t i) const
-		{
-			BOOST_ASSERT(i < particles_.size());
-			return particles_[i];
-		}
-		ParticleType& GetParticle(uint32_t i)
-		{
-			BOOST_ASSERT(i < particles_.size());
-			return particles_[i];
-		}
+		Particle const & GetParticle(uint32_t i) const;
+		Particle& GetParticle(uint32_t i);
 
 	protected:
-		function<void(ParticleType& par, float4x4 const & mat)> gen_func_;
-		function<void(ParticleType& par, float elapse_time)> update_func_;
+		function<void(Particle& par, float4x4 const & mat)> emitter_func_;
+		function<void(Particle& par, float elapse_time)> update_func_;
 
-		std::vector<ParticleType> particles_;
+		std::vector<Particle> particles_;
 
 		float inv_emit_freq_;
 		float accumulate_time_;
@@ -119,15 +83,77 @@ namespace KlayGE
 		float4x4 model_mat_;
 	};
 
-	template <typename ParticleType>
-	class UpdateParticleFunc
+	class KLAYGE_CORE_API ConeParticleEmitter
 	{
 	public:
-		void operator()(ParticleType& par, float elapse_time)
-		{
-			par.pos += par.vel * elapse_time;
-			par.life -= elapse_time;
-		}
+		ConeParticleEmitter();
+
+		void MaxPositionDeviation(float3 const & dev);
+		float3 const & MaxPositionDeviation() const;
+
+		void EmitAngle(float angle);
+		float EmitAngle() const;
+
+		void InitLife(float life);
+		float InitLife() const;
+		void InitSize(float size);
+		float InitSize() const;
+		void InitColor(Color const & clr);
+		Color const & InitColor() const;
+
+		void InitMinVelocity(float min_vel);
+		void InitMaxVelocity(float max_vel);
+
+		void operator()(Particle& par, float4x4 const & mat);
+
+	private:
+		float RandomGen();
+
+	private:
+		ranlux24_base gen_;
+		uniform_int_distribution<> random_dis_;
+
+		float3 max_position_deviation_;
+		float emit_angle_;
+
+		float min_velocity_;
+		float max_velocity_;
+
+		float init_life_;
+		float init_size_;
+		Color init_color_;
+	};
+
+	class KLAYGE_CORE_API ParticleUpdater
+	{
+	public:
+		ParticleUpdater();
+
+		void InitLife(float life);
+
+		void Force(float3 force);
+
+		void MediaDensity(float density);
+
+		void SizeOverLife(std::vector<float2> const & size_over_life);
+		void WeightOverLife(std::vector<float2> const & weight_over_life);
+		void TransparencyOverLife(std::vector<float2> const & transparency_over_life);
+		
+		void ColorFromTo(Color const & from, Color const & to);
+
+		void operator()(Particle& par, float elapse_time);
+
+	private:
+		float init_life_;
+		float gravity_;
+		float3 force_;
+		float media_density_;
+
+		std::vector<float2> size_over_life_;
+		std::vector<float2> weight_over_life_;
+		std::vector<float2> transparency_over_life_;
+
+		Color clr_from_, clr_to_;
 	};
 }
 

KlayGE/Core/Include/KlayGE/PreDeclare.hpp
@@ -185,8 +185,10 @@ namespace KlayGE
 	typedef shared_ptr<TAAPostProcess> TAAPostProcessPtr;
 	class SSVOPostProcess;
 	typedef shared_ptr<SSVOPostProcess> SSVOPostProcessPtr;
-	template <typename ParticleType>
+	struct Particle;
+	typedef shared_ptr<Particle> ParticlePtr;
 	class ParticleSystem;
+	typedef shared_ptr<ParticleSystem> ParticleSystemPtr;
 	class InfTerrainRenderable;
 	typedef shared_ptr<InfTerrainRenderable> InfTerrainRenderablePtr;
 	class InfTerrainSceneObject;

KlayGE/Core/Src/Render/ParticleSystem.cpp
@@ -1,0 +1,301 @@
+/**
+ * @file ParticleSystem.cpp
+ * @author Minmin Gong
+ *
+ * @section DESCRIPTION
+ *
+ * This source file is part of KlayGE
+ * For the latest info, see http://www.klayge.org
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * You may alternatively use this source under the terms of
+ * the KlayGE Proprietary License (KPL). You can obtained such a license
+ * from http://www.klayge.org/licensing/.
+ */
+
+#include <KlayGE/KlayGE.hpp>
+
+#include <KlayGE/ParticleSystem.hpp>
+
+namespace KlayGE
+{
+	ParticleSystem::ParticleSystem(uint32_t max_num_particles,
+					function<void(Particle& par, float4x4 const & mat)> const & emitter_func,
+					function<void(Particle& par, float elapse_time)> const & update_func)
+		: emitter_func_(emitter_func), update_func_(update_func),
+			particles_(max_num_particles),
+			inv_emit_freq_(0), accumulate_time_(0),
+			model_mat_(float4x4::Identity())
+	{
+	}
+
+	void ParticleSystem::ModelMatrix(float4x4 const & model)
+	{
+		model_mat_ = model;
+	}
+
+	float4x4 const & ParticleSystem::ModelMatrix() const
+	{
+		return model_mat_;
+	}
+
+	void ParticleSystem::Frequency(float freq)
+	{
+		inv_emit_freq_ = 1.0f / freq;
+
+		float time = 0;
+		typedef KLAYGE_DECLTYPE(particles_) ParticlesType;
+		KLAYGE_FOREACH(ParticlesType::reference particle, particles_)
+		{
+			particle.life = -1;
+			particle.birth_time = time;
+			time += inv_emit_freq_;
+		}
+	}
+
+	float ParticleSystem::Frequency() const
+	{
+		return 1.0f / inv_emit_freq_;
+	}
+
+	void ParticleSystem::Update(float /*app_time*/, float elapsed_time)
+	{
+		accumulate_time_ += elapsed_time;
+		if (accumulate_time_ >= particles_.size() * inv_emit_freq_)
+		{
+			accumulate_time_ = 0;
+		}
+
+		typedef KLAYGE_DECLTYPE(particles_) ParticlesType;
+		KLAYGE_FOREACH(ParticlesType::reference particle, particles_)
+		{
+			if (particle.life > 0)
+			{
+				update_func_(particle, elapsed_time);
+			}
+			else
+			{
+				float const t = accumulate_time_ - particle.birth_time;
+				if ((t >= 0) && (t < elapsed_time))
+				{
+					emitter_func_(particle, model_mat_);
+				}
+			}
+		}
+	}
+
+	uint32_t ParticleSystem::NumParticles() const
+	{
+		return static_cast<uint32_t>(particles_.size());
+	}
+
+	Particle const & ParticleSystem::GetParticle(uint32_t i) const
+	{
+		BOOST_ASSERT(i < particles_.size());
+		return particles_[i];
+	}
+
+	Particle& ParticleSystem::GetParticle(uint32_t i)
+	{
+		BOOST_ASSERT(i < particles_.size());
+		return particles_[i];
+	}
+
+
+	ConeParticleEmitter::ConeParticleEmitter()
+		: random_dis_(-10000, +10000),
+			emit_angle_(PI / 3), init_life_(3)
+	{
+	}
+
+	void ConeParticleEmitter::MaxPositionDeviation(float3 const & dev)
+	{
+		max_position_deviation_ = dev;
+	}
+
+	float3 const & ConeParticleEmitter::MaxPositionDeviation() const
+	{
+		return max_position_deviation_;
+	}
+
+	void ConeParticleEmitter::EmitAngle(float angle)
+	{
+		emit_angle_ = angle;
+	}
+
+	float ConeParticleEmitter::EmitAngle() const
+	{
+		return emit_angle_;
+	}
+
+	void ConeParticleEmitter::InitLife(float life)
+	{
+		init_life_ = life;
+	}
+
+	float ConeParticleEmitter::InitLife() const
+	{
+		return init_life_;
+	}
+
+	void ConeParticleEmitter::InitSize(float size)
+	{
+		init_size_ = size;
+	}
+
+	float ConeParticleEmitter::InitSize() const
+	{
+		return init_size_;
+	}
+
+	void ConeParticleEmitter::InitColor(Color const & clr)
+	{
+		init_color_ = clr;
+	}
+
+	Color const & ConeParticleEmitter::InitColor() const
+	{
+		return init_color_;
+	}
+
+	void ConeParticleEmitter::InitMinVelocity(float min_vel)
+	{
+		min_velocity_ = min_vel;
+	}
+
+	void ConeParticleEmitter::InitMaxVelocity(float max_vel)
+	{
+		max_velocity_ = max_vel;
+	}
+
+	void ConeParticleEmitter::operator()(Particle& par, float4x4 const & mat)
+	{
+		float x = this->RandomGen() * max_position_deviation_.x();
+		float y = this->RandomGen()* max_position_deviation_.y();
+		float z = this->RandomGen()* max_position_deviation_.z();
+		par.pos = MathLib::transform_coord(float3(x, y, z), mat);
+		float theta = this->RandomGen() * PI;
+		float phi = abs(this->RandomGen()) * emit_angle_ / 2;
+		float velocity = (this->RandomGen() * 0.5f + 0.5f) * (max_velocity_ - min_velocity_) + min_velocity_;
+		float vx = cos(theta) * sin(phi);
+		float vz = sin(theta) * sin(phi);
+		float vy = cos(phi);
+		par.vel = MathLib::transform_normal(float3(vx, vy, vz) * velocity, mat);
+		par.life = init_life_;
+		par.spin = this->RandomGen() * PI / 2;
+		par.size = init_size_;
+		par.color = init_color_;
+	}
+
+	float ConeParticleEmitter::RandomGen()
+	{
+		return MathLib::clamp(random_dis_(gen_) * 0.0001f, -1.0f, +1.0f);
+	}
+
+	ParticleUpdater::ParticleUpdater()
+		: gravity_(0.1f),
+			force_(0, 0, 0),
+			media_density_(0.0f)
+	{
+	}
+
+	void ParticleUpdater::InitLife(float life)
+	{
+		init_life_ = life;
+	}
+
+	void ParticleUpdater::Force(float3 force)
+	{
+		force_ = force;
+	}
+
+	void ParticleUpdater::MediaDensity(float density)
+	{
+		media_density_ = density;
+	}
+
+	void ParticleUpdater::SizeOverLife(std::vector<float2> const & size_over_life)
+	{
+		size_over_life_ = size_over_life;
+	}
+
+	void ParticleUpdater::WeightOverLife(std::vector<float2> const & weight_over_life)
+	{
+		weight_over_life_ = weight_over_life;
+	}
+
+	void ParticleUpdater::TransparencyOverLife(std::vector<float2> const & transparency_over_life)
+	{
+		transparency_over_life_ = transparency_over_life;
+	}
+
+	void ParticleUpdater::ColorFromTo(Color const & from, Color const & to)
+	{
+		clr_from_ = from;
+		clr_to_ = to;
+	}
+
+	void ParticleUpdater::operator()(Particle& par, float elapse_time)
+	{
+		float pos = (init_life_ - par.life) / init_life_;
+
+		float cur_size = size_over_life_.back().y();
+		for (std::vector<float2>::const_iterator iter = size_over_life_.begin(); iter != size_over_life_.end() - 1; ++ iter)
+		{
+			if ((iter + 1)->x() >= pos)
+			{
+				float const s = (pos - iter->x()) / ((iter + 1)->x() - iter->x());
+				cur_size = MathLib::lerp(iter->y(), (iter + 1)->y(), s);
+				break;
+			}
+		}
+
+		float cur_weight = weight_over_life_.back().y();
+		for (std::vector<float2>::const_iterator iter = weight_over_life_.begin(); iter != weight_over_life_.end() - 1; ++ iter)
+		{
+			if ((iter + 1)->x() >= pos)
+			{
+				float const s = (pos - iter->x()) / ((iter + 1)->x() - iter->x());
+				cur_weight = MathLib::lerp(iter->y(), (iter + 1)->y(), s);
+				break;
+			}
+		}
+
+		float cur_alpha = transparency_over_life_.back().y();
+		for (std::vector<float2>::const_iterator iter = transparency_over_life_.begin(); iter != transparency_over_life_.end() - 1; ++ iter)
+		{
+			if ((iter + 1)->x() >= pos)
+			{
+				float const s = (pos - iter->x()) / ((iter + 1)->x() - iter->x());
+				cur_alpha = MathLib::lerp(iter->y(), (iter + 1)->y(), s);
+				break;
+			}
+		}
+
+		float buoyancy = cur_size * media_density_;
+		float3 accel = (force_ + float3(0, buoyancy, 0)) / cur_weight - float3(0, gravity_, 0);
+		par.vel += accel * elapse_time;
+		par.pos += par.vel * elapse_time;
+		par.life -= elapse_time;
+		par.spin += 0.001f;
+		par.size = cur_size;
+		*reinterpret_cast<float4*>(&par.color) = MathLib::lerp(*reinterpret_cast<float4*>(&clr_from_),
+			*reinterpret_cast<float4*>(&clr_to_), pos);
+		par.color.a() = cur_alpha;
+	}
+}

KlayGE/Samples/media/ParticleEditor/Explosive.psml
@@ -5,26 +5,26 @@
 			<to tex="fire.dds"/>
 		</alpha>
 		<color>
-			<from r="1" g="0.45" b="0.25"/>
-			<to r="1" g="0.45" b="0.25"/>
+			<from r="1" g="0.17" b="0.05"/>
+			<to r="1" g="0.17" b="0.05"/>
 		</color>
 	</particle>
-	<emittor angle="281" life="4" min_velocity="0.170000002" max_velocity="0.280000001"/>
+	<emitter angle="281" life="4" min_velocity="0.170000002" max_velocity="0.280000001"/>
 	<max_position_deviation x="0.00999999978" y="0" z="0.00999999978"/>
 	<curve name="size_over_life">
-		<ctrl_point x="0" y="0.266666681"/>
-		<ctrl_point x="0.370000005" y="0.0583333336"/>
-		<ctrl_point x="0.74000001" y="0.0333333351"/>
-		<ctrl_point x="0.845000029" y="0.0333333351"/>
-		<ctrl_point x="1" y="0"/>
+		<ctrl_point x="0" y="0.733333319"/>
+		<ctrl_point x="0.370000005" y="0.9416666664"/>
+		<ctrl_point x="0.74000001" y="0.9666666649"/>
+		<ctrl_point x="0.845000029" y="0.9666666649"/>
+		<ctrl_point x="1" y="1"/>
 	</curve>
 	<curve name="weight_over_life">
-		<ctrl_point x="0" y="0.850000024"/>
-		<ctrl_point x="1" y="1"/>
+		<ctrl_point x="0" y="0.149999976"/>
+		<ctrl_point x="1" y="0"/>
 	</curve>
 	<curve name="transparency_over_life">
 		<ctrl_point x="0" y="0.5"/>
-		<ctrl_point x="1" y="1"/>
+		<ctrl_point x="1" y="0"/>
 	</curve>
 </particle_system>
 

KlayGE/Samples/media/ParticleEditor/Fire.psml
@@ -5,18 +5,18 @@
 			<to tex="fire.dds"/>
 		</alpha>
 		<color>
-			<from r="1" g="0.45" b="0.25"/>
-			<to r="1" g="0.45" b="0.25"/>
+			<from r="1" g="0.17" b="0.05"/>
+			<to r="1" g="0.17" b="0.05"/>
 		</color>
 	</particle>
-	<emittor angle="47" life="2" min_velocity="0.2" max_velocity="0.31"/>
+	<emitter angle="47" life="2" min_velocity="0.2" max_velocity="0.31"/>
 	<max_position_deviation x="0.01" y="0.0" z="0.01"/>
 	<curve name="size_over_life">
-		<ctrl_point x="0" y="0.441666663"/>
-		<ctrl_point x="0.150000006" y="0.24166666"/>
-		<ctrl_point x="0.289999992" y="0.150000006"/>
-		<ctrl_point x="0.779999971" y="0.800000012"/>
-		<ctrl_point x="1" y="1"/>
+		<ctrl_point x="0" y="0.558333337"/>
+		<ctrl_point x="0.150000006" y="0.75833334"/>
+		<ctrl_point x="0.289999992" y="0.849999994"/>
+		<ctrl_point x="0.779999971" y="0.199999988"/>
+		<ctrl_point x="1" y="0"/>
 	</curve>
 	<curve name="weight_over_life">
 		<ctrl_point x="0" y="0.5"/>
@@ -24,7 +24,7 @@
 	</curve>
 	<curve name="transparency_over_life">
 		<ctrl_point x="0" y="0.5"/>
-		<ctrl_point x="1" y="1"/>
+		<ctrl_point x="1" y="0"/>
 	</curve>
 </particle_system>
 

commit 6e0deab50e96d62aa4c635c5382688b22ead182e

Fix some issues found by PVS-Studio #1 (#230)
Fix some issues found by PVS-Studio

* [KFL] Non-void functions should return value

* [DXBCDisasm] Print correct value for shader extensions flag

* [DXBC2GLSL] Reduce struct size on x64 from 24 to 16 bytes

Use highest to lowest size fields order to improve packing.

* [KlayGE\HWDetect] Do not mix wchar_t[] and BSTR they are different

* [KlayGE\UI] Fix clamping for control point value of UIPolylineEditBox

Regression, introduced in aaff35d9ca76f8e842fb057464a46e89ab338dc3

* [KlayGE\Render] Simplify A && B || !A -> !A || B

DXBC2GLSL/Include/DXBC2GLSL/Shader.hpp
@@ -55,8 +55,8 @@ struct TextureSamplerInfo
 
 struct DclIndexRangeInfo
 {
-	ShaderOperandType op_type; 
 	int64_t start;
+	ShaderOperandType op_type; 
 	uint32_t num;
 };
 

DXBC2GLSL/Src/GLSLGen.cpp
@@ -7400,8 +7400,8 @@ void GLSLGen::FindDclIndexRange()
 		if (SO_DCL_INDEX_RANGE == dcl->opcode)
 		{
 			DclIndexRangeInfo mInfo;
-			mInfo.op_type = dcl->op->type;
 			mInfo.start = dcl->op->indices[0].disp;
+			mInfo.op_type = dcl->op->type;
 			mInfo.num = dcl->num;
 			idx_range_info_.push_back(mInfo);
 		}

DXBC2GLSL/Tools/DXBCDisasm/ASMGen.cpp
@@ -286,7 +286,7 @@ void ASMGen::Disasm(std::ostream& out, ShaderDecl const & dcl)
 				out << " enableDoubleExtensions";
 				first_flag = false;
 			}
-			if (dcl.dcl_global_flags.enable_raw_and_structured_in_non_cs)
+			if (dcl.dcl_global_flags.enable_shader_extensions)
 			{
 				if (!first_flag)
 				{

KFL/include/KFL/StringUtil.hpp
@@ -67,11 +67,13 @@ namespace KlayGE
 				EqualToT& operator=(EqualToT const& rhs) noexcept
 				{
 					ch_ = rhs.ch_;
+					return *this;
 				}
 
 				EqualToT& operator=(EqualToT&& rhs) noexcept
 				{
 					ch_ = std::move(rhs.ch_);
+					return *this;
 				}
 
 				bool operator()(CharType ch) const noexcept
@@ -108,11 +110,13 @@ namespace KlayGE
 				IsAnyOfT& operator=(IsAnyOfT const& rhs)
 				{
 					set_ = rhs.set_;
+					return *this;
 				}
 
 				IsAnyOfT& operator=(IsAnyOfT&& rhs) noexcept
 				{
 					set_ = std::move(rhs.set_);
+					return *this;
 				}
 
 				bool operator()(CharType ch) const

KlayGE/Core/Src/Base/HWDetect.cpp
@@ -108,9 +108,10 @@ namespace KlayGE
 		{
 			BOOST_ASSERT(wbem_services_);
 
-			wchar_t query_lang[] = L"WQL";
+			BSTR query_lang = SysAllocString(L"WQL");
 			HRESULT hr = wbem_services_->ExecQuery(query_lang, wql,
 				WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, nullptr, wbem_enum_result_.release_and_put());
+			SysFreeString(query_lang);
 			if (FAILED(hr))
 			{
 				return false;
@@ -224,14 +225,18 @@ namespace KlayGE
 			return false;
 		}
 
-		wchar_t network_resource[] = L"root\\WMI";
-		if (!wmi.ConnectServer(network_resource))
+		BSTR network_resource = SysAllocString(L"root\\WMI");
+		bool ok = wmi.ConnectServer(network_resource);
+		SysFreeString(network_resource);
+		if (!ok)
 		{
 			return false;
 		}
 
-		wchar_t wql[] = L"SELECT * FROM MSSMBios_RawSMBiosTables";
-		if (!wmi.ExecuteQuery(wql))
+		BSTR wql = SysAllocString(L"SELECT * FROM MSSMBios_RawSMBiosTables");
+		ok = wmi.ExecuteQuery(wql);
+		SysFreeString(wql);
+		if (!ok)
 		{
 			return false;
 		}

KlayGE/Core/Src/Render/CameraController.cpp
@@ -127,7 +127,7 @@ namespace KlayGE
 			case TurnLeftRight:
 				{
 					InputMouseActionParamPtr param = checked_pointer_cast<InputMouseActionParam>(action.second);
-					if ((left_button_down_ && (param->buttons_state & MB_Left)) || !left_button_down_)
+					if (!left_button_down_ || (param->buttons_state & MB_Left))
 					{
 						this->RotateRel(param->move_vec.x() * scaler, 0, 0);
 					}
@@ -137,7 +137,7 @@ namespace KlayGE
 			case TurnUpDown:
 				{
 					InputMouseActionParamPtr param = checked_pointer_cast<InputMouseActionParam>(action.second);
-					if ((left_button_down_ && (param->buttons_state & MB_Left)) || !left_button_down_)
+					if (!left_button_down_ || (param->buttons_state & MB_Left))
 					{
 						this->RotateRel(0, param->move_vec.y() * scaler, 0);
 					}

KlayGE/Core/Src/UI/UIPolylineEditBox.cpp
@@ -96,7 +96,7 @@ namespace KlayGE
 	int UIPolylineEditBox::AddCtrlPoint(float pos, float value)
 	{
 		pos = MathLib::clamp(pos, 0.0f, 1.0f);
-		value = MathLib::clamp(pos, 0.0f, 1.0f);
+		value = MathLib::clamp(value, 0.0f, 1.0f);
 
 		int index;
 		if (ctrl_points_.size() >= 1)